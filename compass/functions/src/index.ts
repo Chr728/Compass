77b4343921efba7468a907facff24e11b9c23402
jonabitbol <tbnrsamurai@gmail.com>
2023-08-29 14:57:47 -0400

    added unit tests for cloud functions and temporary edit to pipeline

--- compass/functions/src/index.ts
+++ compass/functions/src/index.ts
@@ -1,18 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import {setGlobalOptions} from "firebase-functions/v2";
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
 
 // Set the maximum instances to 10 for all functions
 setGlobalOptions({maxInstances: 10});
+
+admin.initializeApp();
+
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -21,3 +18,14 @@ export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+//Firestore function to make each newly created document's email lowercase when created
+
+export const makeEmailLowercase = functions.firestore
+    .document("Users/{userId}")
+    .onCreate((snap, context) => {
+        const data = snap.data();
+        const email = data.email.toLowerCase();
+        return snap.ref.set({email}, {merge: true});
+    });
+
---
+++ compass/functions/test/online.test.ts
@@ -0,0 +1,23 @@
+require('dotenv').config()
+import {makeEmailLowercase} from "../src/index";
+
+const testEnv = require('firebase-functions-test')({
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+}, process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+
+describe('Test Firestore Functions', () => {
+    afterAll(() => {
+        testEnv.cleanup();
+    });
+    it('should make email lowercase', async () => {
+        const wrapped = testEnv.wrap(makeEmailLowercase);
+        const snap = testEnv.firestore.makeDocumentSnapshot(
+            {email: 'TEST@GMAIL.COM'}, 'Users/TEST');
+        await wrapped(snap);
+        const data = (await snap.ref.get()).data();
+        expect(data.email).toBe('test@gmail.com');
+        await snap.ref.delete();
+})
+});
