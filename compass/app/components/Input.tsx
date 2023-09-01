fbcbc72d258cc98cc5673658eb66f90c7f5e2ed4
Divleen12 <divleenkahluwalia@gmail.com>
2023-08-31 22:32:45 -0400

    Refactored form using Formik

--- compass/app/components/Input.tsx
+++ compass/app/components/Input.tsx
@@ -6,12 +6,13 @@ type InputProps = {
     id: string;
     type:string;
     value: string;
-    onChange: React.ChangeEventHandler<HTMLElement> | undefined;
+    onChange?: React.ChangeEventHandler<HTMLElement>;
+    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
     style?: CSSProperties;
 }
 
-export default function Input({name, id, type, value, onChange, style} : InputProps) {
+export default function Input({name, id, type, value, onChange, onBlur, style} : InputProps) {
   return (
-    <input name={name} id={id} type={type} value={value} onChange={onChange} className="h-[52px] border border-solid border-lightgrey rounded-md text-grey p-2 focus:outline-blue shadow-[0_4px_8px_0_rgba(44,39,56,0.04)]" style={style}/>
+    <input name={name} id={id} type={type} value={value} onChange={onChange} onBlur={onBlur} className="h-[52px] border border-solid border-lightgrey rounded-md text-grey p-2 focus:outline-blue shadow-[0_4px_8px_0_rgba(44,39,56,0.04)]" style={style}/>
   )
 }
--- compass/app/login/page.tsx
+++ compass/app/login/page.tsx
@@ -4,48 +4,44 @@ import Input from '../components/Input';
 import Button from '../components/Button';
 import Link from 'next/link';
 import Image from 'next/image'
+import { useFormik } from 'formik';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+    const router = useRouter();
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        onSubmit: values => {
+            console.log(values);
+            // redirect to home page on form submission
+            router.push('/');
+        },
+        validate: values => {
+            let errors : {
+                email?: string;
+                password?:string;
+            } = {};
+            if(!values.email){
+                errors.email= "Email is required";
+            }
+            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+                errors.email = 'Invalid email format';
+            }
+
+            if(!values.password){
+                errors.password="Password is required";
+            }
+
+            return errors;
+        }
+    })
+
+ 
     const [visible, setVisible] = useState<boolean>(false);
-
-    
-    const [emailError, setEmailError] = useState<string>();
-    const [passwordError, setPasswordError] = useState<string>();
-    
-    const handleEmail = (e:React.ChangeEvent<HTMLInputElement>): void => {
-        e.preventDefault();
-        setEmail(e.target.value);
-    }
-    const handlePassword = (e:React.ChangeEvent<HTMLInputElement>): void => {
-        e.preventDefault();
-        setPassword(e.target.value);
-    }
-
-    const handleSubmit = (e:React.FormEvent): void => {
-        e.preventDefault();
-        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)){
-            setEmailError("Please enter a valid email address");
-        }
-        else {
-            setEmailError("");
-        }
-        if(password===""){
-            setPasswordError("Please enter a valid password");
-            // console.log(password);
-        }
-        else{
-            setPasswordError("");
-        }
-
-        console.log(passwordError);
-        console.log(emailError);
-        if(emailError==="" && passwordError===""){
-            setEmail("");
-            setPassword("");
-        }
-    }
 
     const handleVisibility = (e:React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
         e.preventDefault();
@@ -54,7 +50,7 @@ export default function Login() {
 
   return (
     <div className="bg-eggshell min-h-screen flex flex-col">
-        <form className="rounded-3xl bg-white flex flex-col m-auto w-full md:max-w-[800px] md:h-[600px] p-8" onSubmit={handleSubmit}>
+        <form className="rounded-3xl bg-white flex flex-col m-auto w-full md:max-w-[800px] md:h-[600px] p-8" onSubmit={formik.handleSubmit}>
                 <div className="mb-6">
                     <p className="text-[34px] text-darkgrey font-sans font-bold">Sign In</p>
                     <p className="text-darkgrey text-[16px] font-sans leading-[22px]">Don&apos;t have an account yet?</p>
@@ -64,15 +60,14 @@ export default function Login() {
                 <div>
                     <label htmlFor='email' className="text-grey font-medium">Email</label>
                     <br/>
-                    <Input name="email" id="email" type="text" value={email} onChange={handleEmail} style={{ width: '100%' }}/>
+                    <Input name="email" id="email" type="text" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} style={{ width: '100%' }}/>
                 </div>
-                {emailError && <p className="text-[16px] text-red font-sans">Please enter a valid email address</p>}
-               
-
+                {formik.touched.email && formik.errors.email ? <p className="text-[16px] text-red font-sans">{formik.errors.email}</p> : null}
+                
                 <div className="relative mt-6">
                     <label htmlFor='password' className="text-grey font-medium">Password</label>
                     <br/>
-                    <Input name="password" id="password" type={visible ? "text":"password"} value={password} onChange={handlePassword} style={{ width: '100%' }}/>
+                    <Input name="password" id="password" type={visible ? "text":"password"} value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} style={{ width: '100%' }}/>
                     <div className="absolute right-2 bottom-3" onClick={handleVisibility}>
                         {visible? <Image
                                 src="/icons/visibleEye.svg"
@@ -88,11 +83,16 @@ export default function Login() {
                                 />}
                     </div>
                 </div>
-                {passwordError && <p className="text-[16px] text-red font-sans">Please enter a valid password</p>}
-
-
+                {formik.touched.password && formik.errors.password ? <p className="text-[16px] text-red font-sans">{formik.errors.password}</p> : null}
+            
                 <div className="md:mt-auto mt-6 w-full">
+                    {formik.errors.password || formik.errors.email ? 
+                    <Button type="submit" text="Sign In" style={{ width: '100%', cursor: 'not-allowed' }} />
+                    : 
                     <Button type="submit" text="Sign In" style={{ width: '100%' }} />
+                     }
+
+                    
                 </div>
 
                 <p className="text-blue font-sans text-[16px] leading-[22px] mb-4 mt-6"><Link href='/reset'>Forgot Password?</Link></p>
