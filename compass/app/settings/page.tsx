0144ec1258e9eb939d5bc2f12246c42af312099c
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-09-05 20:50:18 -0400

    #44 Making the Icons Clickable

--- compass/app/settings/page.tsx
+++ compass/app/settings/page.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import ProfileIcon from "../../public/icons/Mask.svg";
 import Image from "next/image";
 import RedButton from "../components/RedButton";
+import Button from "../components/Button";
+import Link from "next/link";
 
 export default function Setting() {
   return (
     <div className="bg-eggshell min-h-screen flex flex-col">
       <div className="flex items-center ml-6 m-[10px]">
-        <Image
-          src="/icons/LeftArrow.svg"
-          alt="LeftArrow icon"
-          width={12}
-          height={12}
-          className="mr-4"
-        />
+        <Link href="">
+          <Image
+            src="/icons/LeftArrow.svg"
+            alt="LeftArrow icon"
+            width={12}
+            height={12}
+            className="mr-4"
+          />
+        </Link>
         <span className="text-[color:var(--Dark-Grey,#2C2738)] text-2xl not-italic font-bold leading-[normal] font-family: IBM Plex Sans ; ">
           Settings
         </span>
@@ -23,31 +27,37 @@ export default function Setting() {
           Your account
         </span>
         <br></br>
-        <div className="flex items-center">
-          <Image
-            src="/icons/Mask.svg"
-            alt="Profile icon"
-            width={24}
-            height={24}
-            className="mr-3"
-          />
-          <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
-            Your Profile
-          </span>
-        </div>
+        <Link href="">
+          <div className="flex items-center">
+            <Image
+              src="/icons/Mask.svg"
+              alt="Profile icon"
+              width={24}
+              height={24}
+              className="mr-3"
+            />
+
+            <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
+              Your Profile
+            </span>
+          </div>
+        </Link>
+
         <br></br>
-        <div className="flex items-center">
-          <Image
-            src="/icons/vpn_key.svg"
-            alt="Profile icon"
-            width={24}
-            height={24}
-            className="mr-3"
-          />
-          <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
-            Change your password
-          </span>
-        </div>
+        <Link href="">
+          <div className="flex items-center">
+            <Image
+              src="/icons/vpn_key.svg"
+              alt="Profile icon"
+              width={24}
+              height={24}
+              className="mr-3"
+            />
+            <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
+              Change your password
+            </span>
+          </div>
+        </Link>
         <br></br>
         <hr className="h-px w-336 text-darkgrey"></hr>
         <br></br>
@@ -55,33 +65,37 @@ export default function Setting() {
           How you use Compass
         </span>
         <br></br>
-        <div className="flex items-center">
-          <Image
-            src="/icons/NotificationBell.svg"
-            alt="Profile icon"
-            width={24}
-            height={24}
-            className="mr-3"
-          />
-          <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
-            Push notifications
-          </span>
-        </div>
+        <Link href="">
+          <div className="flex items-center">
+            <Image
+              src="/icons/NotificationBell.svg"
+              alt="Profile icon"
+              width={24}
+              height={24}
+              className="mr-3"
+            />
+            <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
+              Push notifications
+            </span>
+          </div>
+        </Link>
         <br></br>
         <hr className="h-px w-336 text-darkgrey"></hr>
         <br></br>
-        <div className="flex items-center">
-          <Image
-            src="/icons/AboutIcon.svg"
-            alt="Profile icon"
-            width={24}
-            height={24}
-            className="mr-3"
-          />
-          <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
-            About Compass
-          </span>
-        </div>
+        <Link href="">
+          <div className="flex items-center">
+            <Image
+              src="/icons/AboutIcon.svg"
+              alt="Profile icon"
+              width={24}
+              height={24}
+              className="mr-3"
+            />
+            <span className="text-2xl not-italic font-bold font-IBM Plex Sans text-darkgrey">
+              About Compass
+            </span>
+          </div>
+        </Link>
         <div className="text-center mt-[160px] ">
           <RedButton
             type="submit"
