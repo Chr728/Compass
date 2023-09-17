54e9d54d901236b83f392771e19fb997c5af32b5
Christina Darstbanian <minasderstep@gmail.com>
2023-09-17 14:23:03 -0400

    #22  test completed for logout

--- compass/app/logout/logout.test.js
+++ compass/app/logout/logout.test.js
@@ -1,17 +1,24 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import Login from "../login/page";
-import { useAuth, AuthProvider } from '../contexts/AuthContext';
+import Logout from "../logout/page";
 
 describe("Returning back to home main page", () => {
-    beforeEach(() => {
-    test("link redirects to main page", async () => {
+
+    test("Logo displayed correctly", async () => {
         render(<Logout/>);
-        const linkElement = screen.getAllByRole("link")[0];
-        expect(linkElement).toHaveAttribute("href", "/");
+        const logoImage = screen.getByAltText("Logo");
+        expect(logoImage).toBeVisible();
     })
 
- })
-
+    test("logout Message displayed", async () => {
+        render(<Logout/>);
+        const message = screen.getByText(/Successfully logged out/i);
+        expect(message).toBeInTheDocument();
+    })
+    test("link redirects to main page", async () => {
+        render(<Logout/>);
+        const linkElement = screen.getByRole("link");
+        expect(linkElement).toHaveAttribute("href", "/");
+    })
 })
