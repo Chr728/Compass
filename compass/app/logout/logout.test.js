0d5abae2ae41bcbd1885a010ca1c6b44d8a994a5
Christina Darstbanian <minasderstep@gmail.com>
2023-09-17 14:28:09 -0400

    #22  commented properly test functions

--- compass/app/logout/logout.test.js
+++ compass/app/logout/logout.test.js
@@ -4,19 +4,21 @@ import '@testing-library/jest-dom';
 import Logout from "../logout/page";
 
 describe("Returning back to home main page", () => {
-
+   // checks and tests whether the logo image is displayed correctly
     test("Logo displayed correctly", async () => {
         render(<Logout/>);
         const logoImage = screen.getByAltText("Logo");
         expect(logoImage).toBeVisible();
     })
 
-    
+   // checks and tests whether the successfully logged out messsage is displayed correctly
     test("logout Message displayed", async () => {
         render(<Logout/>);
         const message = screen.getByText(/Successfully logged out/i);
         expect(message).toBeInTheDocument();
     })
+
+    // checks and tests whether the link redirects to the main page properly 
     test("link redirects to main page", async () => {
         render(<Logout/>);
         const linkElement = screen.getByRole("link");
