dec0b124665b5781f9bdc63f7fb1c4e514d106c0
jonabitbol <tbnrsamurai@gmail.com>
2023-09-20 19:14:15 -0400

    added dockerization for dev environment

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: './../../.env',
+});
 
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
   test: {
