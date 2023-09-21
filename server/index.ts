bb02ca4f030fbb7b06a4924a6d4002804d06b7ed
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-09-20 22:44:12 -0400

    added my local db #87

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,20 @@
-import express from "express";
-import cors from "cors";
-import db from "./models";
+import express from 'express';
+import cors from 'cors';
+import db from './models';
+require('dotenv').config({ path: './../.env' });
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 //Connection to postgreSQL
 db.sequelize.sync().then(() => {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
   });
 });
