e6d1a409c7b8b4649455998b8290d88155f627ab
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-09-28 14:08:06 -0400

    #29 #103 Adding routes for appointment

---
+++ server/controllers/appointmentController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { Logger } from "../middlewares/logger";
+import db from "../models";
+
+export const getAppointments = async (req: Request, res: Response) => {};
+
+export const createAppointment = async (req: Request, res: Response) => {};
