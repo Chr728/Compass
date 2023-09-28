582cb9c57c292058a65a22d7e60f7beb1880d5d0
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-09-28 15:04:11 -0400

    #29 #103 Adding view and add appointment controller

--- server/controllers/appointmentController.ts
+++ server/controllers/appointmentController.ts
@@ -2,6 +2,58 @@ import { Request, Response } from "express";
 import { Logger } from "../middlewares/logger";
 import db from "../models";
 
-export const getAppointments = async (req: Request, res: Response) => {};
+export const getAppointments = async (req: Request, res: Response) => {
+  try {
+    const userEmail = req.params.email;
+    const userAppointments = await db.Appointment.findAll({
+      where: {
+        email: userEmail,
+      },
+    });
 
-export const createAppointment = async (req: Request, res: Response) => {};
+    if (!userAppointments) {
+      return res.status(404).json({
+        status: "ERROR",
+        message: `No appointment was found for this user.`,
+      });
+    } else {
+      res.status(200).json({
+        status: `SUCCESS`,
+        data: userAppointments,
+      });
+    }
+  } catch (err) {
+    Logger.error(`Error occurred while fetching appointment for user: ${err}`);
+    res.status(400).json({
+      status: `ERROR`,
+      message: `Error getting appointments of user : ${err}`,
+    });
+  }
+};
+
+export const createAppointment = async (req: Request, res: Response) => {
+  try {
+    const email = req.params.email;
+    const { appointmentWith, reason, date, time, notes } = req.body;
+    //Will add the appointmentValidator
+    //appointmentValidator(req.body)
+    const createdAppointment = await db.Appointment.create({
+      email,
+      appointmentWith,
+      reason,
+      date,
+      time,
+      notes,
+    });
+    res.status(201).json({
+      status: `SUCCESS`,
+      data: createdAppointment,
+    });
+  } catch (err) {
+    Logger.error(`Error occurred while creating appointment: ${err}`);
+    res.status(400).json({
+      status: `ERROR`,
+      message: `Error creating appointment record: ${err}`,
+    });
+  }
+};
--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 import db from "./models";
 import userRoutes from "./routes/userRoutes";
-import Morgan from './middlewares/morgan';
-import { Logger } from './middlewares/logger';
-require('dotenv').config({
-  path: './../.env',
+import Morgan from "./middlewares/morgan";
+import { Logger } from "./middlewares/logger";
+import appointmentRoutes from "./routes/appointmentRoutes";
+require("dotenv").config({
+  path: "./../.env",
 });
 
 const app = express();
@@ -15,23 +16,23 @@ app.use(cors());
 app.use(Morgan);
 
 app.use("/api/users", userRoutes);
+app.use("/api/appointments", appointmentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 //Connection to postgreSQL
-if(process.env.NODE_ENV !== 'test'){
-  db.sequelize.sync({ alter: true }).then(()=>{
-    Logger.info("Database Synchronized")
+if (process.env.NODE_ENV !== "test") {
+  db.sequelize.sync({ alter: true }).then(() => {
+    Logger.info("Database Synchronized");
   });
- 
+
   app.listen(process.env.SERVER_DEV_PORT, () => {
-  Logger.info(`Server listening on port ${process.env.SERVER_DEV_PORT || 8000}`);
+    Logger.info(
+      `Server listening on port ${process.env.SERVER_DEV_PORT || 8000}`
+    );
   });
- 
 }
-
-
 
 export default app;
