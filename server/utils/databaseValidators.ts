e137e4aac960d6c504298c93764bac156526b4bb
Jiayi Chen <cjy0427615@gmail.com>
2023-10-03 19:14:08 -0400

    commented the removed addresses to fix the test in pipeline
    the pipeline was failed because of the removed addresses, commented the addresses part in databaseValidator to pass the tests

--- server/utils/databaseValidators.ts
+++ server/utils/databaseValidators.ts
@@ -2,7 +2,8 @@ import {Logger} from '../middlewares/logger';
 import {UserAttributes} from '../models/user';
 
 const userValidator = (values : UserAttributes) => {
-    const {email, firstName, lastName, streetAddress, city, province,postalCode, phoneNumber} = values;
+    // const {email, firstName, lastName, streetAddress, city, province,postalCode, phoneNumber} = values;
+    const {email, firstName, lastName, phoneNumber} = values;
     //check if email is valid
     if(!email || typeof email !== 'string' || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
         Logger.error(`Invalid email: ${email}`);
@@ -19,25 +20,25 @@ const userValidator = (values : UserAttributes) => {
         throw new Error(`Invalid last name: ${lastName}`);
     }
     //check if street is valid
-    if(!streetAddress || typeof streetAddress!== 'string') {
-        Logger.error(`Invalid street: ${streetAddress}`);
-        throw new Error(`Invalid street: ${streetAddress}`);
-    }
-    //check if city is valid
-    if(!city || typeof city !== 'string') {
-        Logger.error(`Invalid city: ${city}`);
-       throw new Error(`Invalid city: ${city}`);
-    }
-    //check if province is valid
-    if(!province || typeof province !== 'string') {
-        Logger.error(`Invalid province: ${province}`);
-        throw new Error(`Invalid province: ${province}`);
-    }
-    //check if postal code is valid
-    if (!postalCode || typeof postalCode !== 'string' || !/[A-Z][0-9][A-Z] ?[0-9][A-Z][0-9]$/i.test(postalCode)){
-        Logger.error(`Invalid postal code: ${postalCode}`);
-        throw new Error(`Invalid postal code: ${postalCode}`);
-    }
+    // if(!streetAddress || typeof streetAddress!== 'string') {
+    //     Logger.error(`Invalid street: ${streetAddress}`);
+    //     throw new Error(`Invalid street: ${streetAddress}`);
+    // }
+    // //check if city is valid
+    // if(!city || typeof city !== 'string') {
+    //     Logger.error(`Invalid city: ${city}`);
+    //    throw new Error(`Invalid city: ${city}`);
+    // }
+    // //check if province is valid
+    // if(!province || typeof province !== 'string') {
+    //     Logger.error(`Invalid province: ${province}`);
+    //     throw new Error(`Invalid province: ${province}`);
+    // }
+    // //check if postal code is valid
+    // if (!postalCode || typeof postalCode !== 'string' || !/[A-Z][0-9][A-Z] ?[0-9][A-Z][0-9]$/i.test(postalCode)){
+    //     Logger.error(`Invalid postal code: ${postalCode}`);
+    //     throw new Error(`Invalid postal code: ${postalCode}`);
+    // }
     //check if phone number is valid
     if(!phoneNumber || typeof phoneNumber !== 'string' || !/^[0-9]{10}$/.test(phoneNumber)) {
         Logger.error(`Invalid phone number: ${phoneNumber}`);
