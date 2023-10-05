d7e0ec0a2533f1c7d640cdbc34dae74a6abf7725
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-10-05 13:31:56 -0400

    refactored the weightJournalAPI to use the new backend route protection

--- client/app/http/weightJournalAPI.ts
+++ client/app/http/weightJournalAPI.ts
@@ -1,9 +1,8 @@
 import { auth } from '../config/firebase';
 
 // Function to make a GET request to retrieve all weight journals for a user
-export async function getWeightJournals(userId: string): Promise<any> {
+export async function getWeightJournals(): Promise<any> {
   try {
-
     const currentUser = auth.currentUser;
     if (!currentUser) {
       throw new Error('No user is currently signed in.');
@@ -12,17 +11,18 @@ export async function getWeightJournals(userId: string): Promise<any> {
     const token = await currentUser.getIdToken();
 
     const response = await fetch(
-      `http://localhost:8000/api/journals/weight/user/${id}`, {
+      `http://localhost:8000/api/journals/weight/user/${id}`,
+      {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        }
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
     if (!response.ok) {
       throw new Error(
-        `Failed to retrieve weight journals for user ${userId}. HTTP Status: ${response.status}`
+        `Failed to retrieve weight journals for user. HTTP Status: ${response.status}`
       );
     }
     const data = await response.json();
@@ -34,12 +34,8 @@ export async function getWeightJournals(userId: string): Promise<any> {
 }
 
 // Function to make a GET request to retrieve a specific weight journal entry
-export async function getWeightJournal(
-  userId: string,
-  weightJournalId: string
-): Promise<any> {
+export async function getWeightJournal(weightJournalId: string): Promise<any> {
   try {
-
     const currentUser = auth.currentUser;
     if (!currentUser) {
       throw new Error('No user is currently signed in.');
@@ -47,17 +43,18 @@ export async function getWeightJournal(
     const token = await currentUser.getIdToken();
 
     const response = await fetch(
-      `http://localhost:8000/api/journals/weight/${weightJournalId}`, {
+      `http://localhost:8000/api/journals/weight/${weightJournalId}`,
+      {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        }
-        }
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
     if (!response.ok) {
       throw new Error(
-        `Failed to retrieve weight journal entry ${weightJournalId} for user ${userId}. HTTP Status: ${response.status}`
+        `Failed to retrieve weight journal entry ${weightJournalId} for user. HTTP Status: ${response.status}`
       );
     }
     const data = await response.json();
@@ -70,11 +67,9 @@ export async function getWeightJournal(
 
 // Function to make a POST request to create a new weight journal entry
 export async function createWeightJournal(
-  userId: string,
   weightJournalData: any
 ): Promise<any> {
   try {
-
     const currentUser = auth.currentUser;
     if (!currentUser) {
       throw new Error('No user is currently signed in.');
@@ -95,7 +90,7 @@ export async function createWeightJournal(
     );
     if (!response.ok) {
       throw new Error(
-        `Failed to create weight journal entry for user ${userId}. HTTP Status: ${response.status}`
+        `Failed to create weight journal entry for user. HTTP Status: ${response.status}`
       );
     }
     const data = await response.json();
@@ -108,12 +103,10 @@ export async function createWeightJournal(
 
 // Function to make a PUT request to update an existing weight journal entry
 export async function updateWeightJournal(
-  userId: string,
   weightJournalId: string,
   updatedWeightJournalData: any
 ): Promise<any> {
   try {
-
     const currentUser = auth.currentUser;
     if (!currentUser) {
       throw new Error('No user is currently signed in.');
@@ -126,14 +119,14 @@ export async function updateWeightJournal(
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(updatedWeightJournalData),
       }
     );
     if (!response.ok) {
       throw new Error(
-        `Failed to update weight journal entry ${weightJournalId} for user ${userId}. HTTP Status: ${response.status}`
+        `Failed to update weight journal entry ${weightJournalId} for user. HTTP Status: ${response.status}`
       );
     }
     const data = await response.json();
@@ -146,11 +139,9 @@ export async function updateWeightJournal(
 
 // Function to make a DELETE request to delete a weight journal entry
 export async function deleteWeightJournal(
-  userId: string,
   weightJournalId: string
 ): Promise<any> {
   try {
-
     const currentUser = auth.currentUser;
     if (!currentUser) {
       throw new Error('No user is currently signed in.');
@@ -162,13 +153,13 @@ export async function deleteWeightJournal(
       {
         method: 'DELETE',
         headers: {
-          'Authorization': `Bearer ${token}`,
-        }
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
     if (!response.ok) {
       throw new Error(
-        `Failed to delete weight journal entry ${weightJournalId} for user ${userId}. HTTP Status: ${response.status}`
+        `Failed to delete weight journal entry ${weightJournalId} for user. HTTP Status: ${response.status}`
       );
     }
     return { message: 'Weight journal entry deleted successfully' };
