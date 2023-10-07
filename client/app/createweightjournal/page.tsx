ebb8f5f5711e1a1975530557232417872622f45b
Christina Darstbanian <minasderstep@gmail.com>
2023-10-07 14:34:36 -0400

    #38  error messages updated to take into account some edge cases in the create weight journal fields

--- client/app/createweightjournal/page.tsx
+++ client/app/createweightjournal/page.tsx
@@ -54,6 +54,60 @@ export default function CreateWeightJournal() {
     },
   });
 
+
+  const [dateError, setDateError] = useState(false);
+const [timeError, setTimeError] = useState(false);
+const [weightError, setWeightError] = useState(false);
+const [heightError, setHeightError] = useState(false);
+const [unitError, setUnitError] = useState(false);
+
+const handleSubmit = (error: any) => {
+  error.preventDefault();
+
+  // Check if Date is empty
+  if (!formik.values.date) {
+    setDateError(true);
+  } else {
+    setDateError(false);
+  }
+
+  // Check if Time is empty
+  if (!formik.values.time) {
+    setTimeError(true);
+  } else {
+    setTimeError(false);
+  }
+
+  // Check if Date is empty
+  if (!formik.values.weight) {
+    setDateError(true);
+  } else {
+    setDateError(false);
+  }
+
+  // Check if Time is empty
+  if (!formik.values.height) {
+    setTimeError(true);
+  } else {
+    setTimeError(false);
+  }
+
+// Check if Date is empty
+if (!formik.values.date) {
+  setDateError(true);
+} else {
+  setDateError(false);
+}
+
+// Check if Time is empty
+if (!formik.values.unit) {
+  setTimeError(true);
+} else {
+  setTimeError(false);
+}
+
+};
+
   return (
     <div className="bg-eggshell min-h-screen flex flex-col">
       <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-4">
@@ -70,111 +124,145 @@ export default function CreateWeightJournal() {
         Create Weight Journal
       </span>
       <form
-        className="rounded-3xl bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[550px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
-        onSubmit={formik.handleSubmit}
-      >
-        <div className="mt-3 mb-3">
-          <label
-            htmlFor="date"
-            className="font-sans font-medium text-grey text-[16px]"
-          >
-            Date
-          </label>
-          <br />
-          <Input
-            name="date"
-            id="date"
-            type="date"
-            style={{ width: '100%' }}
-            onChange={formik.handleChange}
-            value={formik.values.date}
-            onBlur={formik.handleBlur}
-          />
-        </div>
-<div className="mt-3">
-              <label
-                htmlFor="time"
-                className="font-sans font-medium text-grey text-[16px]"
-              >
-                Time
-              </label>
-              <br />
-              <Input
-                name="time"
-                id="time"
-                type="time"
-                style={{ width: '100%' }}
-                onChange={formik.handleChange}
-                value={formik.values.time}
-                onBlur={formik.handleBlur}
-              />
-            </div>
+      className="rounded-3xl bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[550px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
+      onSubmit={formik.handleSubmit}
+    >
+      <div className="mt-3 mb-3">
+        <label
+          htmlFor="date"
+          className="font-sans font-medium text-grey text-[16px]"
+        >
+          Date
+        </label>
+        <br />
+        <Input 
+    name="date"
+    id="date"
+    type="date"
+    style={{ width: '100%' }}
+    onChange={formik.handleChange}
+    value={formik.values.date}
+    onBlur={formik.handleBlur}
+    required={true} 
+    />
+{formik.touched.date && !formik.values.date && (
+        <p className="text-red text-[14px]">This field can't be left empty.</p>
+      )}      </div>
 
-            <div className="mt-3">
-              <label
-                htmlFor="weight"
-                className="font-sans font-medium text-grey text-[16px]"
-              >
-                Weight
-              </label>
-              <br />
-              <Input
-                name="weight"
-                id="weight"
-                type="number"
-                style={{ width: '100%' }}
-                onChange={formik.handleChange}
-                value={formik.values.weight.toString()}
-                onBlur={formik.handleBlur}
-              />
-               {formik.touched.weight && formik.values.weight === 0 && (
-          <p className="text-red text-[14px]">You can't enter a weight of zero.</p>
+      <div className="mt-3">
+        <label
+          htmlFor="time"
+          className="font-sans font-medium text-grey text-[16px]"
+        >
+          Time
+        </label>
+        <br />
+        <Input
+    name="time"
+    id="time"
+    type="time"
+    style={{ width: '100%' }}
+    onChange={formik.handleChange}
+    value={formik.values.time}
+    onBlur={formik.handleBlur}
+  />
+{formik.touched.time && !formik.values.time && (
+        <p className="text-red text-[14px]">This field can't be left empty.</p>
+      )}      
+      </div>
+
+      <div className="mt-3">
+        <label
+          htmlFor="weight"
+          className="font-sans font-medium text-grey text-[16px]"
+        >
+          Weight
+        </label>
+        <br />
+        <Input
+          name="weight"
+          id="weight"
+          type="number"
+          style={{ width: '100%' }}
+          onChange={formik.handleChange}
+          value={formik.values.weight.toString()}
+          onBlur={formik.handleBlur}
+        />
+         {/* Check if the field is touched */}
+  {formik.touched.weight && (
+    // Check if the field is empty
+    !formik.values.weight && (
+      <p className="text-red text-[14px]">This field can't be left empty or zero.</p>
+    ) || (
+      // Check if the field is less than or equal to zero
+      formik.values.weight <= 0 && (
+        <p className="text-red text-[14px]">You can't enter a negative weight or a weight of zero.</p>
+      )
+    )
+  )}
+      </div>
+
+      <div className="mt-3">
+        <label
+          htmlFor="height"
+          className="font-sans font-medium text-grey text-[16px]"
+        >
+          Height
+        </label>
+        <br />
+        <Input
+          name="height"
+          id="height"
+          type="number"
+          style={{ width: '100%' }}
+          onChange={formik.handleChange}
+          value={formik.values.height.toString()}
+          onBlur={formik.handleBlur}
+        />
+        
+
+       {/* Check if the field is touched */}
+  {formik.touched.height && (
+    // Check if the field is empty
+    !formik.values.height && (
+      <p className="text-red text-[14px]">This field can't be left empty or zero.</p>
+    ) || (
+      // Check if the field is less than or equal to zero
+      formik.values.height <= 0 && (
+        <p className="text-red text-[14px]">You can't enter a negative weight or a weight of zero.</p>
+      )
+    )
+  )}
+      
+      </div>
+
+      <div className="mt-3">
+        <label
+          htmlFor="unit"
+          className="font-sans font-medium text-grey text-[16px]"
+        >
+          Unit
+        </label>
+        <br />
+        <select
+          name="unit"
+          id="unit"
+          style={{ width: '100%' }}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.unit}
+        >
+          <option value="kg">kg</option>
+          <option value="lb">lb</option>
+        </select>
+        {unitError && <p className="text-red text-[14px]">This field can't be left empty.</p>}
+
+        {formik.touched.unit && !formik.values.unit && (
+          <p className="text-red text-[14px]">This field can't be left empty.</p>
         )}
-            </div>
+      </div>
 
-            <div className="mt-3">
-              <label
-                htmlFor="height"
-                className="font-sans font-medium text-grey text-[16px]"
-              >
-               Height
-              </label>
-              <br />
-              <Input
-                name="height"
-                id="height"
-                type="number"
-                style={{ width: '100%' }}
-                onChange={formik.handleChange}
-                value={formik.values.height.toString()}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.height && formik.values.height === 0 && (
-          <p className="text-red text-[14px]">You can't enter a height of zero.</p>
-        )}
-            </div>
-
-
-            <div className="mt-3">
-              <label
-                htmlFor="Unit"
-                className="font-sans font-medium text-grey text-[16px]"
-              >
-                Unit
-              </label>
-              <br />
-              <Input
-                name="unit"
-                id="unit"
-                type="text"
-                style={{ width: '100%' }}
-                onChange={formik.handleChange}
-                value={formik.values.unit}
-                onBlur={formik.handleBlur}
-              />
-            </div>
-
-            <div className="mt-3">
+      <div className="mt-3">
                 <label
                   htmlFor="notes"
                   className="font-sans font-medium text-grey text-[16px]"
@@ -192,26 +280,34 @@ export default function CreateWeightJournal() {
                   onBlur={formik.handleBlur}
                 />
               </div>
+      
+      <div className="mx-auto space-x-2">
+        <Button
+          type="button"
+          text="Cancel"
+          style={{ width: '140px', backgroundColor: 'var(--Red, #FF7171)' }}
+          onClick={() => router.push("/profile")}
+        />
 
-              
-
-            <div className="mx-auto space-x-2">
-              <Button
-                type="button"
-                text="Cancel"
-                style={{ width: '140px', backgroundColor: 'var(--Red, #FF7171)' }}
-                onClick={ () => router.push("/profile")}
-              />
-
-      <Button
+<Button
           type="submit"
           text="Submit"
-          disabled={!(formik.isValid && formik.dirty) || formik.values.weight === 0 || formik.values.height === 0}
+          disabled={
+            !(formik.isValid && formik.dirty) || // Check if the form is valid and dirty
+            formik.values.weight === 0 || // Check if weight is zero
+            formik.values.weight < 0 || // Check if weight is less than  zero
+            formik.values.height === 0 || // Check if height is zero
+            formik.values.height < 0 || // Check if height is less than zero
+            !formik.values.unit || // Check if unit is missing or empty
+            !formik.values.date || // Check if date is missing or empty
+            !formik.values.time || // Check if time is missing or empty
+            !formik.values.weight || // Check if weight is missing or empty
+            !formik.values.height // Check if height is missing or empty
+          }
           style={{ width: '140px' }}
         />
-            </div>
-
-      </form>
+      </div>
+    </form>
     </div>
   );
 
