b31f2bca0a7b18ae5d4d092cbebff722154f117b
MrMelon1232 <julien.phan18@gmail.com>
2023-10-10 18:01:26 -0400

    #12 Added button id to custom button component

--- server/notifications/webPush.ts
+++ server/notifications/webPush.ts
@@ -1,17 +1,19 @@
-const webPush = require('web-push');
+// const webPush = require('web-push');
 
-//VAPID keys
-webPush.setVapidDetails(
-  'todo', //email
-  'todo', //public key
-  'todo'//private key
-);
+// //VAPID keys
+// webPush.setVapidDetails(
+//   'todo', //email
+//   'todo', //public key
+//   'todo'//private key
+// );
 
-// Send a push notification
-webPush.sendNotification(subscription, JSON.stringify(payload))
-  .then(() => {
-    console.log('Push notification sent successfully');
-  })
-  .catch((error) => {
-    console.error('Error sending push notification:', error);
-  });
+// // Send a push notification
+// webPush.sendNotification(subscription, JSON.stringify(payload))
+//   .then(() => {
+//     console.log('Push notification sent successfully');
+//   })
+//   .catch((error) => {
+//     console.error('Error sending push notification:', error);
+//   });
+
+const notification = document.querySelector("");
