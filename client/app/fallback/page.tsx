56c32fb06042a8e2bcf7180cde01e25dec27818a
MrMelon1232 <julien.phan18@gmail.com>
2023-10-09 23:52:21 -0400

    #49 modified pwa head meta

---
+++ client/app/fallback/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+export default function Fallback() {
+  return (
+    <div>
+      <h1>This section of the app requires an internet connection :/</h1>
+    </div>
+  );
+}
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['gsap'],
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   transpilePackages: ["gsap"],
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  swSrc: "./service-worker.js",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  transpilePackages: ["gsap"],
+});
