5a21516d28a33d583005cea45c3935bd63b3b2a7
Reuven1203 <reuven1203@gmail.com>
2023-10-16 14:36:38 -0400

    optimized menu and fixed asthetic
    also noticed that model was outdated with changes. Fixed that too

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,28 +1,44 @@
+'use client';
 import { AuthProvider } from "./contexts/AuthContext";
 import { UserProvider } from "@/app/contexts/UserContext";
 import type { Metadata } from "next";
 import "./globals.css";
+import {usePathname} from 'next/navigation';
+import React, {useMemo} from 'react';
+import Menu from '@/app/components/Menu';
+import './globals.css';
+const MemoizedMenu = React.memo(Menu);
 
 export const metadata: Metadata = {
-  title: "Compass",
-  description: "Compass health app",
-  manifest: "/manifest.json",
-  themeColor: "#fff",
+    title: "Compass",
+    description: "Compass health app",
+    manifest: "/manifest.json",
+    themeColor: "#fff",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head />
-      <body>
+    const pathname = usePathname();
+    const isLoggedIn = useMemo(() => {
+        return !(pathname === '/login' || pathname === '/register');
+    }, [pathname]);
+
+    return (
+        <html lang="en">
+        <head />
+        <body>
         <AuthProvider>
-          <UserProvider>{children}</UserProvider>
+            <UserProvider>
+                {children}
+                <div className={`xl:max-w-[1280px] w-full  menu-container`}>
+                    {isLoggedIn && <MemoizedMenu />}
+                </div>
+            </UserProvider>
         </AuthProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
