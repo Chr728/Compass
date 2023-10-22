f1050b8bd18fada59bbb7c9c9b6ef81d142dc56f
Christina Darstbanian <minasderstep@gmail.com>
2023-10-22 05:05:44 -0400

    #37  food intake journal created for single page

--- client/app/getFoodJournals/[foodJournal]/page.tsx
+++ client/app/getFoodJournals/[foodJournal]/page.tsx
@@ -5,7 +5,7 @@ import Input from '../../components/Input';
 import Link from 'next/link';
 import { useFormik } from 'formik';
 import { useRouter } from 'next/navigation';
-import { getWeightJournal, getWeightJournals} from '../../http/weightJournalAPI'; 
+import { getFoodIntakeJournal, getFoodIntakeJournals} from '../../http/foodJournalAPI'; 
 import { useAuth } from '../../contexts/AuthContext';
 import { useUser } from '../../contexts/UserContext';
 import { useEffect, useState } from 'react';
@@ -15,31 +15,31 @@ import { formatMilitaryTime } from '@/app/helpers/utils/datetimeformat';
 import Custom403 from '@/app/pages/403';
 
 
-export default function GetWeightJournal({params: { weightJournal } } : { params: { weightJournal: string } }) {
+export default function GetFoodJournal({params: { foodJournal } } : { params: { foodJournal: string } }) {
   const { user } = useAuth();
   const router = useRouter();
   const { userInfo } = useUser();
-  const [weight, setweight] = useState<any>(null);
+  const [food, setfood] = useState<any>(null);
   
-  async function fetchWeightJournal() {
+  async function fetchFoodJournal() {
     try {
       const userId = user?.uid || '';
-      const result = await getWeightJournal(weightJournal);
-      console.log('Weight journal entry retrieved:', result);
-      setweight(result.data);
+      const result = await getFoodIntakeJournal(foodJournal);
+      console.log('Food journal entry retrieved:', result);
+      setfood(result.data);
     } catch (error) {
-      console.error('Error retrieving weight journal entry:', error);
+      console.error('Error retrieving Food journal entry:', error);
     }
-
+  }
   useEffect(() => {
     if (!user) {
       router.push("/login")
       alert('User not found.');
     } 
     if (user) {
-      fetchWeightJournal();
+      fetchFoodJournal();
     }
-  }, [user, weight]);
+  }, [user, food]);
 
   if (!user) {
     return <div><Custom403/></div>
@@ -49,11 +49,11 @@ export default function GetWeightJournal({params: { weightJournal } } : { params
     <div className="bg-eggshell min-h-screen flex flex-col">
        <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-4">
               <button onClick={() => router.back()}>
-              <Header headerText="View the Weight Journal"></Header>
+              <Header headerText="View the Food Journal"></Header>
               </button>
               </span>
      
-        {weight && (
+        {food && (
      <span
      className="rounded-2xl  mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
    >
@@ -64,7 +64,7 @@ export default function GetWeightJournal({params: { weightJournal } } : { params
     Date: 
   </p>
   <p className="text-md ml-2 text-darkgrey">
-    {new Date(weight.date).toISOString().split('T')[0]}
+    {new Date(food.date).toISOString().split('T')[0]}
   </p>
 </div>
        <p
@@ -77,34 +77,34 @@ export default function GetWeightJournal({params: { weightJournal } } : { params
            className="text-md ml-2 text-darkgrey"
            style={{display: 'inline'}}
        >
-         {/* {new Date(`1970-01-01T${weight.time}`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })} */}
-         {formatMilitaryTime(weight.time)}
+         {/* {new Date(`1970-01-01T${food.time}`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })} */}
+         {formatMilitaryTime(food.time)}
        </p>
        <br></br>
        <p
            className="text-lg ml-0 font-sans text-darkgrey  font-bold text-[16px]"
            style={{display: 'inline'}}
        >
-         Weight:
+         Name of Food:
        </p>
        <p
            className="text-md ml-2 text-darkgrey"
            style={{display: 'inline'}}
        >
-         {weight.weight}
+         {food.foodName}
        </p>
        <br></br>
        <p
            className="text-lg ml-0 font-sans text-darkgrey  font-bold text-[16px]"
            style={{display: 'inline'}}
        >
-         Unit:
+          Meal Type:
        </p>
        <p
            className="text-md ml-2 text-darkgrey"
            style={{display: 'inline'}}
        >
-        {weight.unit}
+        {food.mealType}
        </p>
        <br></br>
 
@@ -112,13 +112,13 @@ export default function GetWeightJournal({params: { weightJournal } } : { params
            className="text-lg ml-0 font-sans text-darkgrey font-bold text-[16px]"
            style={{display: 'inline'}}
        >
-         Height:
+         Number of Servings:
        </p>
        <p
            className="text-md ml-2 text-darkgrey"
            style={{display: 'inline'}}
        >
-         {weight.height}
+         {food.servingNumber}
        </p>
 
        <br></br>
@@ -132,23 +132,22 @@ export default function GetWeightJournal({params: { weightJournal } } : { params
            className="text-md ml-2 text-darkgrey"
            style={{display: 'inline'}}
        >
-         {weight.notes}
+         {food.notes}
        </p>
        <br></br>
      </div>
    </div>
     <div className='mt-10 mb-2 items-center'>
-    <Button type="button" text="Edit"style={{ width: '140px' }} onClick={() => router.push(`/getWeightJournals/${weightJournal}/${weightJournal}`)} />
+    <Button type="button" text="Edit"style={{ width: '140px' }} onClick={() => router.push(`/getFoodJournals/${foodJournal}/${foodJournal}`)} />
     <Button
     type="button"
     text="Cancel"
     style={{ width: '140px', backgroundColor: 'var(--Red, #FF7171)',marginLeft:'12px' }}
-    onClick={() => router.push(`/getWeightJournals`)}
+    onClick={() => router.push(`/getFoodJournals`)}
     />
     </div>
       </span>
 )}
     </div>
   );
-}
 }
