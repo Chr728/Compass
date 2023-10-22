8ab9ae516449705e6dca6782cbc0605c22f1fdee
Christina Darstbanian <minasderstep@gmail.com>
2023-10-22 05:06:48 -0400

    #37  single food intake journal test completed

--- client/app/getFoodJournals/[foodJournal]/getFoodJournal.test.js
+++ client/app/getFoodJournals/[foodJournal]/getFoodJournal.test.js
@@ -1,8 +1,8 @@
 import {render, screen, act} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import GetWeightJournal from './page';
-import {getWeightJournal,getWeightJournals} from '../../http/weightJournalAPI';
+import GetFoodJournal from './page';
+import {getFoodIntakeJournal,getFoodIntakeJournals} from '../../http/foodJournalAPI';
 import { useRouter } from "next/navigation";
 import { useUser } from '../../contexts/UserContext';
 
@@ -16,20 +16,20 @@ jest.mock("next/navigation", () => ({
     }
 }));
 
-jest.mock('../../http/weightJournalAPI', () => {
+jest.mock('../../http/foodJournalAPI', () => {
     return {
-        getWeightJournal: () => {
+        getFoodJournal: () => {
             return {
                     success: "SUCCESS",
                     data: 
                         {
-                            id: '1',
+                            uid: '1',
                             date: '2014-01-01',
-                            time: '8:36',
-                            weight: '186',
-                            unit: 'lb',
-                            height: '174',
-                            notes: 'notes'
+                            time: '08:36',
+                            foodName: 'pasta',
+                            servingNumber: 2,
+                            mealType:'Lunch',
+                            Notes : 'I am still Hungry'
                     }
             }
         }
@@ -51,39 +51,39 @@ jest.mock("../../contexts/UserContext", () => {
 
 
 test("User data is displayed correctly", async () => {
-    render(<GetWeightJournal params={{ weightJournal:'1' }}/>);
+    render(<GetFoodJournal params={{ foodJournal:'1' }}/>);
     setTimeout(() => {
         expect(screen.getByText("Date:")).toBeInTheDocument();
         expect(screen.getByText("Time:")).toBeInTheDocument();
-        expect(screen.getByText("Weight:")).toBeInTheDocument();
-        expect(screen.getByText("Height:")).toBeInTheDocument();
-        expect(screen.getByText("Unit:")).toBeInTheDocument();
+        expect(screen.getByText("Name of Food:")).toBeInTheDocument(); 
+        expect(screen.getByText("Meal Type:")).toBeInTheDocument();
+        expect(screen.getByText("Number of Servings:")).toBeInTheDocument();
         expect(screen.getByText("Notes:")).toBeInTheDocument();
         expect(screen.getByText("2014-01-01")).toBeInTheDocument();
         expect(screen.getByText("8h36")).toBeInTheDocument();
-        expect(screen.getByText("186")).toBeInTheDocument();
-        expect(screen.getByText("lb")).toBeInTheDocument();
-        expect(screen.getByText("174")).toBeInTheDocument();
+        expect(screen.getByText("pasta")).toBeInTheDocument();
+        expect(screen.getByText("Lunch")).toBeInTheDocument();
+        expect(screen.getByText("2")).toBeInTheDocument();
         expect(screen.getByText("notes")).toBeInTheDocument();
     }, 1000);
 })
 
 test("Cancel button functions correctly", async() => {
-    render(<GetWeightJournal params={{ weightJournal:'1' }}/>);
+    render(<GetFoodJournal params={{ foodJournal:'1' }}/>);
     setTimeout(() => {
         const cancelButton = screen.getAllByRole('button')[2];
         userEvent.click(cancelButton);
         mockRouter;
-        expect(mockRouter).toHaveBeenCalledWith('/getWeightJournals')
+        expect(mockRouter).toHaveBeenCalledWith('/getFoodJournals')
     }, 1000);
 })
 
 test("Update button functions correctly", async() => {
-    render(<GetWeightJournal params={{ weightJournal:'1' }}/>);
+    render(<GetFoodJournal params={{ foodJournal:'1' }}/>);
     setTimeout(() => {
         const updateButton = screen.getAllByRole('button')[1];
         userEvent.click(updateButton);
         mockRouter;
-        expect(mockRouter).toHaveBeenCalledWith('/editWeightJournals/1')
+        expect(mockRouter).toHaveBeenCalledWith('/editFoodJournals/1')
     }, 1000);
 })
