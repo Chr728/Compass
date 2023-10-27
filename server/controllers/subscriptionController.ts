2882d99ad260799c90a8232fc7de3e60971b17cf
Jiayi Chen <cjy0427615@gmail.com>
2023-10-27 15:01:37 -0400

    Added update/delete subscription controller #190

--- server/controllers/subscriptionController.ts
+++ server/controllers/subscriptionController.ts
@@ -95,7 +95,109 @@ export const getSubscription = async (req: Request, res: Response) => {
 };
 
 export const updateSubscription = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.uid;
+
+    const user = await db.User.findOne({
+      where: {
+          uid: userId
+      }
+  });
+
+    if(!user) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: 'User not found, invalid user uid.',
+          });
+    }
+
+    const getSubscription = await db.Subscription.findOne({
+      where: {
+          uid: userId
+      }
+  });
+
+    if(!getSubscription) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: 'No subscription exist for this user.',
+          });
+    }
+
+    const { subscription } = req.body;
+    await db.Subscription.update({
+      subscription,
+    }, {
+       where: {
+          uid: userId
+       }
+    })
+
+    const getUpdatedSubscription = await db.Subscription.findOne({
+      where: {
+          uid: userId
+      }
+  });
+
+    res.status(200).json({
+      status: `SUCCESS`,
+      data: getUpdatedSubscription,
+    });
+
+  } catch (err) {
+    Logger.error(`Error occurred while deleting appointment: ${err}`);
+    res.status(400).json({
+      status: "ERROR",
+      message: `Error deleting appointment record: ${err}`,
+    });
+  }
 };
 
 export const deleteSubscription= async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.uid;
+
+    const user = await db.User.findOne({
+      where: {
+          uid: userId
+      }
+  });
+
+    if(!user) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: 'User not found, invalid user uid.',
+          });
+    }
+
+    const getSubscription = await db.Subscription.findOne({
+      where: {
+          uid: userId
+      }
+  });
+
+    if(!getSubscription) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: 'No subscription exist for this user.',
+          });
+    }
+
+    await db.Subscription.destroy({
+      where: {
+        uid: userId,
+      },
+    });
+
+    res.status(200).json({
+      status: `SUCCESS`,
+      data: `Successfully deleted appointment.`,
+    });
+  } catch (err) {
+    Logger.error(`Error occurred while deleting appointment: ${err}`);
+    res.status(400).json({
+      status: "ERROR",
+      message: `Error deleting appointment record: ${err}`,
+    });
+  }
 };
