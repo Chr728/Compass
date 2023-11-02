953079292b38ee9eaba1e1192b39f6d4b8bd537f
Christina Darstbanian <minasderstep@gmail.com>
2023-11-01 22:18:05 -0400

    #35 create and get glucose journals  pages created

---
+++ client/app/getGlucoseJournals/getDiabeticJournalsPage.tsx
@@ -0,0 +1,134 @@
+'use client';
+import Image from 'next/image';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import Link from 'next/link';
+import { useFormik } from 'formik';
+import { useRouter } from 'next/navigation';
+import { deleteGlucoseJournal, getGlucoseJournal, getGlucoseJournals} from '../http/diabeticJournalAPI'; 
+import { useAuth } from '../contexts/AuthContext';
+import { useUser } from '../contexts/UserContext';
+import { useEffect, useState } from 'react';
+import { MdDeleteForever, MdInfoOutline, MdKeyboardArrowDown } from 'react-icons/md';
+import Header from '../components/Header';
+import { formatDate, formatMilitaryTime } from '../helpers/utils/datetimeformat';
+
+
+export default function GetGlucoseJournalsPage() {
+  const router = useRouter();
+  const { user } = useAuth();
+  const { userInfo } = useUser();
+  const [glucose, setglucose] = useState<any>(null);
+  
+  useEffect(() => {
+    if (!userInfo) {
+      alert('User not found.');
+    } 
+  }, [userInfo, router]);
+
+
+  useEffect(() => {
+    async function fetchGlucoseJournals() {
+      try {
+        const userId = user?.uid || '';
+        const result = await getGlucoseJournals();    
+        console.log('All Glucose journals entry retrieved:', result);
+        setglucose(result.data);
+      } catch (error) {
+        console.error('Error retrieving glucose journal entry:', error);
+      }
+    }
+    fetchGlucoseJournals();
+  }, [user]);
+
+
+    async function deleteGlucoseJournals(glucoseJournalId: string){
+      const deleteresult = await deleteGlucoseJournal(glucoseJournalId);   
+      location.reload();
+    }
+
+
+    return (
+      <div className="bg-eggshell min-h-screen flex flex-col">
+        <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-4">
+          <button onClick={() => router.push('/journals')}>
+            <Header headerText="Glucose Journals "></Header>
+          </button>
+        </span>
+        <p className="font-sans text-darkgrey ml-5 text-[14px]">Keep track of your insulin doses and glucose measurements to ensure a healthy lifestyle.</p>
+        <br></br>    
+        {glucose && (
+  <div className="rounded-3xl bg-white flex flex-col mt-4 mb-6 w-full md:max-w-[800px] md:min-h-[550px] p-4 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
+    <div className="flex justify-between items-center">
+      <div>
+        <Button type="button" text="Add an Entry" style={{ width: '120px', fontSize: '14px', padding: '1px 10px' }} onClick={() => router.push(`/createGlucoseJournal`)} />
+      </div>
+      <div className="flex items-center">
+        <p className="font-sans text-darkgrey ml-2 font-bold text-[14px]">Your height:</p>
+        {glucose.length > 0 && glucose[0].height && (
+          <p className="font-sans text-darkgrey mr-8 font-medium text-[14px]">{glucose[glucose.length - 1].height}cm</p>
+        )}
+      </div>
+    </div>
+    <br></br>
+<div className="flex" style={{ justifyContent: 'space-between' }}>
+    <div className="flex-2" style={{ marginRight: '10%' }}>
+      <div className="font-sans  text-darkgrey font-bold text-[18px] text-center">
+        Date
+        <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />
+      </div>
+    </div>
+    <div className="flex-2" style={{ marginRight: '2%' }}>
+      <div className="font-sans  text-darkgrey font-bold text-[18px] text-center">
+        Meal
+        <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />
+      </div>
+    </div>
+    <div className="flex-2" style={{ marginRight: '10%' }}>
+      <div className="font-sans  text-darkgrey font-bold text-[18px] text-center">
+        Glucose
+        <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />
+      </div>
+    </div>
+  </div>
+  {glucose.map((item: any, index: number) => (
+    <div
+      key={item.glucoseJournalId}
+      className={`flex justify-between items-center mt-3`}
+      style={{
+        backgroundColor: index % 2 === 0 ? 'white' : '#DBE2EA',
+      }}
+      onClick={() => router.push(`/getGlucoseJournals/${item.id}`)}
+    >
+      <div className="flex-2">
+        <p className="font-sans font-medium text-darkgrey text-[14px] text-center">
+        {`${formatDate(item.date)}`}
+        </p>
+      </div>
+      <div className="flex-2">
+        <p className="ml-3 font-sans font-medium text-darkgrey text-[14px] text-center">
+          {item.mealType}
+        </p>
+      </div>
+      <div className="flex-2">
+        <p className="ml-4 font-sans font-medium text-darkgrey text-[14px] text-center">
+          {item.glucose}
+        </p>
+      </div>
+      
+      <div className="flex icons" style={{ marginLeft: '5px', marginRight: '5px' }}>
+        <div className="icon" id= "Trash Icon">
+          <MdDeleteForever
+            style={{ color: 'var(--Red, #FF7171)', width: '25px', height: '30px' }}
+            onClick={() => deleteGlucoseJournals(item.id)}
+          />
+        </div>
+      </div>
+    </div>
+  ))}
+  </div>
+)}
+      </div>
+    );
+    
+}
---
+++ client/app/getGlucoseJournals/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import Custom403 from '../pages/403';
+import GetDiabeticJournalsPage from "./getDiabeticJournalsPage";
+
+export default function GetWeightJournals() {
+  const router = useRouter();
+  const { user } = useAuth()
+
+  React.useEffect(() => {
+    if (!user) 
+      router.push("/login")
+  }, [user])
+
+  if (!user) {
+    return <div><Custom403/></div>
+  }
+
+  return (
+    <GetDiabeticJournalsPage />  );
+}
