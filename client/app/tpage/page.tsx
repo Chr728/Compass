0a5597f4b39515e434ecc0a7b13f833d116723f5
Divleen12 <divleenkahluwalia@gmail.com>
2023-11-04 15:48:19 -0400

    #208 Redesigned dashboard

--- client/app/tpage/page.tsx
+++ client/app/tpage/page.tsx
@@ -4,6 +4,11 @@ import Menu from '../components/Menu';
 import { useRouter } from "next/navigation";
 import { useAuth } from "../contexts/AuthContext";
 import Custom403 from '../pages/403';
+import {
+  MdEditNote,
+  MdCalendarMonth
+} from 'react-icons/md';
+import Image from 'next/image';
 
 export default function MainMenu() {
   const { user } = useAuth()
@@ -32,6 +37,9 @@ return (
         onClick = {() => router.push('/viewappointments')}
       >
           Appointments
+          <div className="flex text-2xl justify-center">
+            <MdCalendarMonth/>
+          </div>
       </div>
         
       <div 
@@ -39,20 +47,40 @@ return (
         onClick = {() => router.push('/')}
       >
         Medications
+        <div className="flex justify-center">
+          <Image
+            src="/icons/medication.svg"
+            alt="Medicine icon"
+            width={20}
+            height={20}
+          />
+        </div>
       </div>
 
       <div 
-      className="bg-yellow rounded-xl h-36 py-14 bg-opacity-90 hover:-translate-y-1.5"
+      className="bg-yellow rounded-xl h-36 py-14 bg-opacity-90 hover:translate-y-1.5"
       onClick = {() => router.push('/journals')}
       >
         Journals
+        <div className="flex text-3xl justify-center">
+          <MdEditNote/>
+        </div>
+        
       </div>
 
       <div 
-        className="bg-green rounded-xl h-36 py-14 bg-opacity-90 hover:-translate-y-1.5"
+        className="bg-green rounded-xl h-36 py-14 bg-opacity-90 hover:translate-y-1.5"
         onClick = {() => router.push('/profile')}
       >
         Profile
+        <div className="flex justify-center">
+          <Image
+            src="/icons/whiteMask.svg"
+            alt="Profile icon"
+            width={18}
+            height={18}
+          />
+        </div>
       </div>
 
     </div>
