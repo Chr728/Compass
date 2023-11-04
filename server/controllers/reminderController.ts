ef2c866598c01587005669952bd107c17370757c
MrMelon1232 <julien.phan18@gmail.com>
2023-11-03 20:07:53 -0400

    Added proper path for .env file for reminder Controller

--- server/controllers/reminderController.ts
+++ server/controllers/reminderController.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./../../.env" });
 import { Request, Response } from "express";
 import { Logger } from "../middlewares/logger";
 import db from "../models";
