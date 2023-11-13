6ec1f812a66129406ddf5f24075e88235b6fe371
Jan Ong <janong24@gmail.com>
2023-11-13 08:42:12 -0500

    Logger exporting on hold

--- client/logger.js
+++ client/logger.js
@@ -1,5 +1,3 @@
-// import { default as Pino } from 'pino';
-// import Pino from 'pino';
 const pino = require('pino');
 
 const levels = {
@@ -11,25 +9,16 @@ const levels = {
   fatal: 60,
 };
 
-// const fileTransport = pino.transport({
-//   target: 'pino/file',
-//   options: { destination: `${__dirname}/app.log`, append: true },
+// const transport = pino.transport({
+//   targets: [{
+//     target: '#pino/file',
+//     options: {
+//       destination: `${__dirname}/app.log`
+//     }
+//   }]
 // });
-// pino(transport);
 
-// module.exports = Pino(
-//   {
-//     customLevels: levels, 
-//     useOnlyCustomLevels: true,
-//     level: 'info',
-//     prettyPrint: {
-//       colorize: true, 
-//       levelFirst: true,
-//       translateTime: 'yyyy-dd-mm, h:MM:ss TT',
-//     },
-//   },
-//   fileTransport
-// );
+// const logger = pino(transport);
 
 module.exports = pino(
   {
@@ -40,20 +29,5 @@ module.exports = pino(
       levelFirst: true,
       translateTime: 'yyyy-dd-mm, h:MM:ss TT',
     },
-  }
+  },
 );
-
-// const logger = pino( {
-//     customLevels: levels, 
-//     useOnlyCustomLevels: true,
-//     level: 'info',
-//     prettyPrint: {
-//       colorize: true, 
-//       levelFirst: true,
-//       translateTime: 'yyyy-dd-mm, h:MM:ss TT',
-//     },
-//   },
-//   // fileTransport
-// );
-
-// export default logger;
