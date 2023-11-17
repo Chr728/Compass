ca4235d983f9bfd06a84138a770066ad9eaff72c
Jiayi Chen <cjy0427615@gmail.com>
2023-11-17 18:47:37 -0500

    Added update and delete medication controller #12

--- server/controllers/medicationController.ts
+++ server/controllers/medicationController.ts
@@ -109,7 +109,89 @@ export const getMedication = async (req: Request,res: Response) => {
 };
 
 export const updateMedication = async (req: Request,res: Response) => {
+    try {
+        const medicationId = req.params.id;
+        const medication = await db.Medication.findOne({
+            where: {
+                id: medicationId
+            }
+        });
+
+        if(!medication) {
+            return res.status(404).json({
+                status: 'ERROR',
+                message: 'Medication not found, invalid medication id.',
+              });
+        }
+
+        const { medicationName, dateStarted, time, dosage, unit, frequency, route, notes } = req.body;
+
+        await db.Medication.update({
+            medicationName,
+            dateStarted,
+            time,
+            dosage,
+            unit,
+            frequency,
+            route,
+            notes,
+        }, {
+            where: {
+                id: medicationId
+            }
+        })
+
+        const updatedMedication = await db.Medication.findOne({
+            where: {
+                id: medicationId
+            }
+        });
+
+        return res.status(200).json({
+            status: 'SUCCESS',
+            data: updatedMedication
+        });
+
+    } catch (error) {
+        Logger.error(`Error occurred while updating medication: ${error}`);
+        return res.status(400).json({
+            status: 'ERROR',
+            message: `Error updating medications: ${error}`,
+        });
+    }
 };
 
 export const deleteMedication = async (req: Request,res: Response) => {
+    try {
+        const medicationId = req.params.id;
+        const medication = await db.Medication.findOne({
+            where: {
+                id: medicationId
+            }
+        });
+
+        if(!medication) {
+            return res.status(404).json({
+                status: 'ERROR',
+                message: 'Medication not found, invalid medication id.',
+              });
+        }
+        await db.Medication.destroy({
+            where: {
+                id: medicationId
+            }
+        });
+
+        return res.status(200).json({
+            status: 'SUCCESS',
+            message: 'Medication entry deleted successfully',
+        });
+
+    } catch (error) {
+        Logger.error(`Error occurred while deleting medication record: ${error}`);
+        return res.status(400).json({
+            status: 'ERROR',
+            message: `Error deleting medication record: ${error}`,
+        });
+    }
 };
