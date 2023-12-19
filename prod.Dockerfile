34785198ef99a235b21f7c8dc56b4ab577043aeb
jonabitbol <tbnrsamurai@gmail.com>
2023-12-19 13:49:37 -0500

    fixes for production

--- prod.Dockerfile
+++ prod.Dockerfile
@@ -1,17 +1,19 @@
 FROM node:18-bullseye-slim AS server_production
 
 ARG PORT
-ARG PORT
 ENV PORT ${PORT}
 ARG DATABASE_URL
 ENV DATABASE_URL ${DATABASE_URL}
+ARG VAPID_PUBLIC_KEY
+ENV VAPID_PUBLIC_KEY ${VAPID_PUBLIC_KEY}
+ARG VAPID_PRIVATE_KEY
+ENV VAPID_PRIVATE_KEY ${VAPID_PRIVATE_KEY}
 ENV NODE_ENV production
 
 WORKDIR /app/server/
 
 COPY server/package*.json ./
 COPY server/tsconfig.json ./
-COPY server/serviceAccountKey.json ./
 
 RUN npm ci
 
--- server/config/firebase.ts
+++ server/config/firebase.ts
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
-const path = require('path');
-const serviceAccount = require(path.resolve(__dirname,'../serviceAccountKey.json'))
+// const path = require('path');
+// const serviceAccount = require(path.resolve(__dirname,'../serviceAccountKey.json'))
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SA as string))
 })
 
 export default admin;
