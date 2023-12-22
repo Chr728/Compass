f8de3196104d7a67472d27966ae51406c9199197
jonabitbol <tbnrsamurai@gmail.com>
2023-12-21 23:33:40 -0500

    removed useless default route in server

--- server/index.ts
+++ server/index.ts
@@ -47,11 +47,6 @@ app.use('/api/subscription', subscriptionRoutes);
 app.use('/api/reminders', reminderRoutes);
 app.use('/api/medication', medicationRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-
 if (isDevelopment) {
   db.sequelize.sync({alter: true}).then(() => {
     Logger.info('Database Synchronized');
