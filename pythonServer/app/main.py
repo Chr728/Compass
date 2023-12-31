3f532ca0ad65945bd4b506e0196b081fe9674eac
Jiayi Chen <cjy0427615@gmail.com>
2023-12-31 01:18:38 -0500

    added code for reading env variables for the server #289
    A new package named python-dotenv is needed. Two env variable will be read: PYTHON_HOST and PYTHON_PORT

--- pythonServer/app/main.py
+++ pythonServer/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PYTHON_PORT = int(os.getenv("PYTHON_PORT"))
+PYTHON_HOST = os.getenv("PYTHON_HOST")
 
 app = FastAPI()
 
@@ -9,4 +16,4 @@ async def read_root():
     return {'message': 'Hello World'}
 
 if __name__ == "__main__":
-    uvicorn.run(app, debug=True)
+    uvicorn.run(app, host=PYTHON_HOST, port =PYTHON_PORT)
