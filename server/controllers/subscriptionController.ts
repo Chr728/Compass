f414104aa2438ddd05b3bf889fd5eedb59fcb5ad
Reuven1203 <reuven1203@gmail.com>
2023-12-31 17:15:41 -0500

    refactor subscription controller for error middleware
    Fix some tests

--- server/controllers/subscriptionController.ts
+++ server/controllers/subscriptionController.ts
@@ -1,203 +1,118 @@
-import { Request, Response } from 'express';
-import { Logger } from '../middlewares/logger';
-import db from '../models';
+import { Request, Response, NextFunction } from "express";
+import { Logger } from "../middlewares/logger";
+import db from "../models";
+import { ErrorHandler } from '../middlewares/errorMiddleware';
 
-export const createSubscription = async (req: Request, res: Response) => {
-  try {
-    const userId = req.params.uid;
-    const user = await db.User.findOne({
-        where: {
-            uid: userId
+export const createSubscription = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.params.uid;
+        const user = await db.User.findOne({ where: { uid: userId } });
+
+        if (!user) {
+            throw new ErrorHandler(404, 'ERROR', 'User not found, invalid user uid.');
         }
-    });
 
-    if(!user) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'User not found, invalid user uid.',
-          });
-    }
+        const getSubscription = await db.Subscription.findOne({ where: { uid: userId } });
 
-    const getSubscription = await db.Subscription.findOne({
-      where: {
-          uid: userId
-      }
-  });
+        if (getSubscription) {
+            throw new ErrorHandler(400, 'ERROR', 'The user already has one subscription, please update instead!');
+        }
 
-    if(getSubscription) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'The user already has one subscription, please update instead!',
-          });
-    }
+        const { subscription } = req.body;
+        const createSubscription = await db.Subscription.create({ uid: userId, subscription });
 
-  const { subscription } = req.body;
-  const createSubscription = await db.Subscription.create({
-    uid: userId,
-    subscription: subscription
-  });
-
-  return res.status(201).json({
-      status: 'SUCCESS',
-      data: createSubscription
-  });
-
-    
-} catch (error) {
+        return res.status(201).json({ status: 'SUCCESS', data: createSubscription });
+    } catch (error) {
         Logger.error(`Error occurred while creating subscription: ${error}`);
-            return res.status(400).json({
-            status: 'ERROR',
-            message: `Error creating subscription: ${error}`,
-        });
-    }
-  };
-
-export const getSubscription = async (req: Request, res: Response) => {
-  try{
-    const userId = req.params.uid;
-    const user = await db.User.findOne({
-        where: {
-            uid: userId
+        if (error instanceof ErrorHandler) {
+            next(error);
+        } else {
+            next(new ErrorHandler(400, 'ERROR', `Error creating subscription: ${error}`));
         }
-    });
-
-    if(!user) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'User not found, invalid user uid.',
-          });
     }
-
-    const getSubscription = await db.Subscription.findOne({
-      where: {
-          uid: userId
-      }
-  });
-
-    if(!getSubscription) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'No subscription exist for this user, please create.',
-          });
-    }
-
-    return res.status(200).json({
-        status: 'SUCCESS',
-        data: getSubscription
-    });
-} catch (error) {
-    Logger.error(`Error occurred while fetching subscription: ${error}`);
-    return res.status(400).json({
-        status: 'ERROR',
-        message: `Error fetching subscription: ${error}`,
-    });
-}
 };
 
-export const updateSubscription = async (req: Request, res: Response) => {
-  try {
-    const userId = req.params.uid;
+export const getSubscription = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.params.uid;
+        const user = await db.User.findOne({ where: { uid: userId } });
 
-    const user = await db.User.findOne({
-      where: {
-          uid: userId
-      }
-  });
+        if (!user) {
+            throw new ErrorHandler(404, 'ERROR', 'User not found, invalid user uid.');
+        }
 
-    if(!user) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'User not found, invalid user uid.',
-          });
+        const getSubscription = await db.Subscription.findOne({ where: { uid: userId } });
+
+        if (!getSubscription) {
+            throw new ErrorHandler(404, 'ERROR', 'No subscription exist for this user, please create.');
+        }
+
+        return res.status(200).json({ status: 'SUCCESS', data: getSubscription });
+    } catch (error) {
+        Logger.error(`Error occurred while fetching subscription: ${error}`);
+        if (error instanceof ErrorHandler) {
+            next(error);
+        } else {
+            next(new ErrorHandler(400, 'ERROR', `Error fetching subscription: ${error}`));
+        }
     }
-
-    const getSubscription = await db.Subscription.findOne({
-      where: {
-          uid: userId
-      }
-  });
-
-    if(!getSubscription) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'No subscription exist for this user.',
-          });
-    }
-
-    const { subscription } = req.body;
-    await db.Subscription.update({
-      subscription,
-    }, {
-       where: {
-          uid: userId
-       }
-    })
-
-    const getUpdatedSubscription = await db.Subscription.findOne({
-      where: {
-          uid: userId
-      }
-  });
-
-    res.status(200).json({
-      status: `SUCCESS`,
-      data: getUpdatedSubscription,
-    });
-
-  } catch (err) {
-    Logger.error(`Error occurred while updating subscription: ${err}`);
-    res.status(400).json({
-      status: "ERROR",
-      message: `Error updating subscription record: ${err}`,
-    });
-  }
 };
 
-export const deleteSubscription= async (req: Request, res: Response) => {
-  try {
-    const userId = req.params.uid;
+export const updateSubscription = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.params.uid;
+        const user = await db.User.findOne({ where: { uid: userId } });
 
-    const user = await db.User.findOne({
-      where: {
-          uid: userId
-      }
-  });
+        if (!user) {
+            throw new ErrorHandler(404, 'ERROR', 'User not found, invalid user uid.');
+        }
 
-    if(!user) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'User not found, invalid user uid.',
-          });
+        const getSubscription = await db.Subscription.findOne({ where: { uid: userId } });
+
+        if (!getSubscription) {
+            throw new ErrorHandler(404, 'ERROR', 'No subscription exist for this user.');
+        }
+
+        const { subscription } = req.body;
+        await db.Subscription.update({ subscription }, { where: { uid: userId } });
+
+        const getUpdatedSubscription = await db.Subscription.findOne({ where: { uid: userId } });
+
+        res.status(200).json({ status: `SUCCESS`, data: getUpdatedSubscription });
+    } catch (error) {
+        Logger.error(`Error occurred while updating subscription: ${error}`);
+        if (error instanceof ErrorHandler) {
+            next(error);
+        } else {
+            next(new ErrorHandler(400, 'ERROR', `Error updating subscription record: ${error}`));
+        }
     }
+};
 
-    const getSubscription = await db.Subscription.findOne({
-      where: {
-          uid: userId
-      }
-  });
+export const deleteSubscription = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.params.uid;
+        const user = await db.User.findOne({ where: { uid: userId } });
 
-    if(!getSubscription) {
-        return res.status(404).json({
-            status: 'ERROR',
-            message: 'No subscription exist for this user.',
-          });
+        if (!user) {
+            throw new ErrorHandler(404, 'ERROR', 'User not found, invalid user uid.');
+        }
+
+        const getSubscription = await db.Subscription.findOne({ where: { uid: userId } });
+
+        if (!getSubscription) {
+            throw new ErrorHandler(404, 'ERROR', 'No subscription exist for this user.');
+        }
+
+        await db.Subscription.destroy({ where: { uid: userId } });
+
+        res.status(200).json({ status: `SUCCESS`, message: `Successfully deleted subscription.` });
+    } catch (error) {
+        Logger.error(`Error occurred while deleting subscription: ${error}`);
+        if (error instanceof ErrorHandler) {
+            next(error);
+        } else {
+            next(new ErrorHandler(400, 'ERROR', `Error deleting subscription: ${error}`));
+        }
     }
-
-    await db.Subscription.destroy({
-      where: {
-        uid: userId,
-      },
-    });
-
-    res.status(200).json({
-      status: `SUCCESS`,
-      message: `Successfully deleted subscription.`,
-    });
-  } catch (err) {
-    Logger.error(`Error occurred while deleting subscription: ${err}`);
-    res.status(400).json({
-      status: "ERROR",
-      message: `Error deleting subscription: ${err}`,
-    });
-  }
 };
