40069412f6f4b732afad35d97c28e6a10a946636
Jiayi Chen <cjy0427615@gmail.com>
2024-01-03 22:25:56 -0500

    Added tests for the PillAI endpoint #289
    Two test suits were added for the two branches.

--- pythonServer/app/test_main.py
+++ pythonServer/app/test_main.py
@@ -9,3 +9,19 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello World'}
+
+def test_pill_predict_wrong_file_format():
+    test_file = 'test_files/test.txt'
+    files = {'file': ('test.txt', open(test_file, 'rb'))}
+    response = client.post('/PillAI', files=files)
+    assert response.status_code == 404
+    assert response.json() == {"message": "Image format must jpg, jpeg or png!"}
+
+def test_pill_predict():
+    test_file = 'test_files/0.jpg'
+    files = {'file': ('0.jpg', open(test_file, 'rb'))}
+    response = client.post('/PillAI', files=files)
+    assert response.status_code == 200
+    assert response.json() == {"prediction": "your prediction", 
+                               "filename": "0.jpg"}
+
