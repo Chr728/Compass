3332332e63d8cdf8c58c57abde0e978f8671f019
Reuven1203 <reuven1203@gmail.com>
2024-01-03 19:28:06 -0500

    remove consolelog

--- client/app/contexts/AuthContext.tsx
+++ client/app/contexts/AuthContext.tsx
@@ -79,8 +79,6 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
       handlePopUp('error', 'Error signing out:' + error.message);
     }
   };
-  console.log('token ',auth.currentUser?.getIdToken())
-
   const signUp = (values: createUserAttributes) => {
     handleLoading(true);
     createUserWithEmailAndPassword(auth, values.email, values.password)
