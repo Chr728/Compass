8a65cebe9c185e429f50dd7bddcd1a7372959deb
Christina Darstbanian <minasderstep@gmail.com>
2024-01-08 23:07:47 -0500

    #276 added pill identifer page test file

---
+++ client/app/pillIdentifier/pillIdentifierPage.test.js
@@ -0,0 +1,171 @@
+import "@testing-library/jest-dom";
+import { act, cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+beforeEach(async () => {
+	await act(async () => {
+		render(<pillIdentifierPage />);
+	});
+});
+
+afterEach(() => {
+	cleanup();
+});
+
+const mockRouter = jest.fn();
+jest.mock("next/navigation", () => ({
+	useRouter: () => {
+		return {
+			push: mockRouter,
+		};
+	},
+}));
+
+test("Cancel  button  functions correctly", async () => {
+	setTimeout(() => {
+		const cancelButton = screen.getAllByRole("button")[3];
+		userEvent.click(cancelButton);
+		mockRouter;
+		expect(mockRouter).toHaveBeenCalledWith("/getMedications");
+	}, 1000);
+});
+
+test("Message displayed", async () => {
+	setTimeout(() => {
+		const route = screen.findByText(
+			"Use our advanced AI to identify any pills or tablets you may have in hand."
+		);
+		expect(route).toBeInTheDocument();
+	}, 1000);
+});
+
+test("Message displayed", async () => {
+	setTimeout(() => {
+		const route = screen.findByText(
+			"The app doesn't provide a 100% guarantee when identifying medications."
+		);
+		expect(route).toBeInTheDocument();
+	}, 1000);
+});
+
+test("Message displayed", async () => {
+	setTimeout(() => {
+		const route = screen.findByText(
+			"The app provides you a score of the closest match."
+		);
+		expect(route).toBeInTheDocument();
+	}, 1000);
+});
+
+test("Message displayed", async () => {
+	setTimeout(() => {
+		const route = screen.findByText(
+			"Take pictures against a clear background."
+		);
+		expect(route).toBeInTheDocument();
+	}, 1000);
+});
+
+test("Capture  button  functions correctly", async () => {
+	setTimeout(() => {
+		const captureButton = screen.getAllByRole("button")[1];
+
+		// Mock getUserMedia to simulate camera access
+		const mockMediaDevices = {
+			getUserMedia: jest.fn(() => Promise.resolve({})),
+		};
+		global.navigator.mediaDevices = mockMediaDevices;
+
+		// Click the Take a picture button
+		userEvent.click(captureButton);
+
+		// Ensure that the camera starts (is active)
+		expect(mockMediaDevices.getUserMedia).toHaveBeenCalled();
+
+		// Simulate capturing an image
+		const canvas = screen.getByTestId("hidden-canvas");
+		Object.defineProperty(canvas, "toDataURL", {
+			value: jest.fn(() => "mockImageData"),
+		});
+
+		userEvent.click(screen.getByText("Take a image"));
+
+		// Ensure that the image is captured
+		expect(canvas.toDataURL).toHaveBeenCalled();
+		expect(screen.getByText("Submit")).toBeInTheDocument();
+	}, 1000);
+});
+
+test("renders Upload from gallery button and captures image on file upload", async () => {
+	setTimeout(() => {
+		// Check that the button is initially rendered
+		const uploadButton = screen.getAllByRole("button")[2];
+
+		userEvent.click(uploadButton);
+
+		// Simulate uploading an image
+		const fileInput = screen.getByLabelText(/upload from gallery/i);
+		const imageFile = new File(["(binary)"], "example.png", {
+			type: "image/png",
+		});
+
+		userEvent.change(fileInput, { target: { files: [imageFile] } });
+
+		// Ensure that the image is captured
+		expect(screen.getByAltText("Logo")).toBeInTheDocument();
+		expect(screen.getByText("Submit")).toBeInTheDocument();
+	}, 1000);
+});
+
+test("renders captured image on the screen after capturing", async () => {
+	setTimeout(() => {
+		// Check that the button is initially rendered
+		const captureButton = screen.getAllByRole("button")[1];
+
+		// Mock getUserMedia to simulate camera access
+		const mockMediaDevices = {
+			getUserMedia: jest.fn(() => Promise.resolve({})),
+		};
+		global.navigator.mediaDevices = mockMediaDevices;
+
+		// Click the Take a picture button
+		userEvent.click(captureButton);
+
+		// Ensure that the camera starts (is active)
+		expect(mockMediaDevices.getUserMedia).toHaveBeenCalled();
+
+		// Simulate capturing an image
+		const canvas = screen.getByTestId("hidden-canvas");
+		Object.defineProperty(canvas, "toDataURL", {
+			value: jest.fn(() => "mockImageData"),
+		});
+
+		userEvent.click(screen.getByText("Take a picture"));
+
+		// Ensure that the captured image is rendered on the screen
+		expect(screen.getByAltText("Captured Image")).toBeInTheDocument();
+	}, 1000);
+});
+
+test("renders uploaded image on the screen after uploading", async () => {
+	setTimeout(() => {
+		// Check that the button is initially rendered
+		const uploadButton = screen.getAllByRole("button")[2];
+		// Mock the file input to simulate file selection
+		const fileInput = screen.getByLabelText(
+			"Upload from gallery"
+		).nextSibling;
+		const mockFile = new File(["(⌐□_□)"], "mockImage.png", {
+			type: "image/png",
+		});
+		Object.defineProperty(fileInput, "files", {
+			value: [mockFile],
+		});
+
+		// Click the Upload from gallery button
+		userEvent.click(uploadButton);
+
+		// Ensure that the uploaded image is rendered on the screen
+		expect(screen.getByAltText("Uploaded Image")).toBeInTheDocument();
+	}, 1000);
+});
