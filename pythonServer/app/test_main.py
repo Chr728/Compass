6064b5d8381b4c19912a8f5f16237f3385865091
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2024-01-10 01:58:56 -0500

    Removed the response of the prediction in test for now until our model is fully trained

--- pythonServer/app/test_main.py
+++ pythonServer/app/test_main.py
@@ -8,32 +8,6 @@ from . import main
 
 client = TestClient(app)
 
-sample_output = {
-    "predictions": [
-        {
-            "score": 4.124554634094238,
-            "class_label": "nematode, nematode worm, roundworm"
-        },
-        {
-            "score": 3.9820611476898193,
-            "class_label": "matchstick"
-        },
-        {
-            "score": 3.347282886505127,
-            "class_label": "revolver, six-gun, six-shooter"
-        },
-        {
-            "score": 3.32959246635437,
-            "class_label": "bassoon"
-        },
-        {
-            "score": 3.3281443119049072,
-            "class_label": "oboe, hautboy, hautbois"
-        }
-    ],
-    "filename": "0.jpg"
-}
-
 def fake_predict(filename):
     raise Exception("new Exception")
 
@@ -57,9 +31,7 @@ def test_pill_predict():
     test_file = './test_files/0.jpg'
     files = {'file': ('0.jpg', open(test_file, 'rb'))}
     response = client.post('/PillAI', files=files)
-    print(f"Response JSON: {response.json()}")
     assert response.status_code == 200
-    assert response.json() == sample_output
 
     
 def test_test():
