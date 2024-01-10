105f0e7e6d7d1ffa63d91e2c57aa803978791cac
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2024-01-10 01:43:41 -0500

    Missing Pillow Library in the requirements.txt file for test to pass on pipeline

--- pythonServer/app/test_main.py
+++ pythonServer/app/test_main.py
@@ -57,9 +57,7 @@ def test_pill_predict():
     test_file = './test_files/0.jpg'
     files = {'file': ('0.jpg', open(test_file, 'rb'))}
     response = client.post('/PillAI', files=files)
-    print(f"Response status code: {response.status_code}")
     print(f"Response JSON: {response.json()}")
-    print(f"Files used: {files}")
     assert response.status_code == 200
     assert response.json() == sample_output
 
