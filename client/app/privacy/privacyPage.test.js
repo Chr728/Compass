88c20ca8038fd9358fd6423e80acd259cca6552a
Christina Darstbanian <minasderstep@gmail.com>
2024-01-22 18:44:12 -0500

    #279 making sure privacy page is accessed only if user is authenticated

---
+++ client/app/privacy/privacyPage.test.js
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+test("data is displayed correctly", async () => {
+	render(<aboutUsPage />);
+	setTimeout(() => {
+		expect(
+			screen.getByText(
+				"Compass is a medical wellness app that targets specifically people above 40 years old who might be interested in having some type of assistance to keep their healthy habits and lifestyles. Compass offers features of managing medical reminders, booking appointments,tracking user’s medications and treatments all in one consolidated application. In addition, having features of medical journals such as diabetic journals allows some patients to easily note their daily doses and treatment details making them able to follow the history of their treatments and use it as a reference for themselves or to show to their medical professional. Additionally,with the speed dial fast option to contact relatives during some emergency situations patients would be able to contact their relatives in a faster and easier way. With many features compass aims for users to be healthier and function hassle free."
+			)
+		).toBeInTheDocument();
+	}, 1000);
+});
+
+test("data is displayed correctly", async () => {
+	render(<aboutUsPage />);
+	setTimeout(() => {
+		expect(screen.getByText("About Compass")).toBeInTheDocument();
+	}, 1000);
+});
--- client/app/settings/settings.test.js
+++ client/app/settings/settings.test.js
@@ -1,11 +1,11 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { useRouter } from "next/navigation";
 import SettingsPage from "./settingsPage";
-import "@testing-library/jest-dom";
 
 //Mock useRouter from next/navigation
 jest.mock("next/navigation", () => ({
-  useRouter: jest.fn(),
+	useRouter: jest.fn(),
 }));
 
 //Mock push from next Router
@@ -13,9 +13,9 @@ const mockRouterPush = jest.fn();
 
 const mockLogOut = jest.fn();
 jest.mock("../contexts/AuthContext", () => ({
-  useAuth: () => ({
-    logout: mockLogOut,
-  }),
+	useAuth: () => ({
+		logout: mockLogOut,
+	}),
 }));
 
 //Mock back from next Router
@@ -23,57 +23,62 @@ const mockRouterBack = jest.fn();
 
 //Setitng useRouter mock behaviour before executing tests
 beforeAll(() => {
-  useRouter.mockReturnValue({
-    query: {},
-    push: mockRouterPush,
-    back: mockRouterBack,
-  });
+	useRouter.mockReturnValue({
+		query: {},
+		push: mockRouterPush,
+		back: mockRouterBack,
+	});
 });
 
 describe("Settings Page", () => {
-  //Test to check if page is rendered correctly with proper text and button
-  test("Renders correct content and button", async () => {
-    render(<SettingsPage />);
-    const SettingsHeader = screen.getAllByText(/Settings/i)[0];
-    const YourAccountHeader = screen.getByText(/Your account/i);
-    const YourProfile = screen.getByText(/Your Profile/i);
-    const ChangeYourPassword = screen.getByText(/Change your password/i);
-    const HowToUse = screen.getByText(/How you use Compass/i);
-    const PushNotifications = screen.getByText(/Push notifications/i);
-    const AboutCompass = screen.getByText(/About Compass/i);
-    const MoreInfo = screen.getByText(/More Info/i);
-    const BackButton = screen.getAllByRole("button")[0];
-    const LogoutButton = screen.getAllByRole("button")[1];
+	//Test to check if page is rendered correctly with proper text and button
+	test("Renders correct content and button", async () => {
+		render(<SettingsPage />);
+		const SettingsHeader = screen.getAllByText(/Settings/i)[0];
+		const YourAccountHeader = screen.getByText(/Your account/i);
+		const YourProfile = screen.getByText(/Your Profile/i);
+		const ChangeYourPassword = screen.getByText(/Change your password/i);
+		const HowToUse = screen.getByText(/How you use Compass/i);
+		const PushNotifications = screen.getByText(/Push notifications/i);
+		const AboutCompass = screen.getByText(/About Compass/i);
+		const Privacy = screen.getByText(/Privacy Policy/i);
+		const MoreInfo = screen.getByText(/More Info/i);
+		const BackButton = screen.getAllByRole("button")[0];
+		const LogoutButton = screen.getAllByRole("button")[1];
 
-    expect(SettingsHeader).toBeInTheDocument();
-    expect(YourAccountHeader).toBeInTheDocument();
-    expect(YourProfile).toBeInTheDocument();
-    expect(ChangeYourPassword).toBeInTheDocument();
-    expect(HowToUse).toBeInTheDocument();
-    expect(PushNotifications).toBeInTheDocument();
-    expect(AboutCompass).toBeInTheDocument();
-    expect(MoreInfo).toBeInTheDocument();
+		expect(SettingsHeader).toBeInTheDocument();
+		expect(YourAccountHeader).toBeInTheDocument();
+		expect(YourProfile).toBeInTheDocument();
+		expect(ChangeYourPassword).toBeInTheDocument();
+		expect(HowToUse).toBeInTheDocument();
+		expect(PushNotifications).toBeInTheDocument();
+		expect(AboutCompass).toBeInTheDocument();
+		expect(Privacy).toBeInTheDocument();
+		expect(MoreInfo).toBeInTheDocument();
 
-    expect(BackButton).toBeInTheDocument();
-    fireEvent.click(BackButton);
+		expect(BackButton).toBeInTheDocument();
+		fireEvent.click(BackButton);
 
-    await mockRouterPush;
-    expect(mockRouterPush).toHaveBeenCalledTimes(1);
+		await mockRouterPush;
+		expect(mockRouterPush).toHaveBeenCalledTimes(1);
 
-    expect(LogoutButton).toBeInTheDocument();
-    fireEvent.click(LogoutButton);
-  });
+		expect(LogoutButton).toBeInTheDocument();
+		fireEvent.click(LogoutButton);
+	});
 
-  // test("Check if button navigates to logout page", () => {
-  //   render(<SettingsPage />);
-  //   const LogoutButton = screen.getByText("Sign Out"); // Assuming the button text is 'Sign Out'
-  //   fireEvent.click(LogoutButton);
-  //   expect(mockLogOut).toHaveBeenCalled(); // Check if the logout function is called
-  // });
+	// test("Check if button navigates to logout page", () => {
+	//   render(<SettingsPage />);
+	//   const LogoutButton = screen.getByText("Sign Out"); // Assuming the button text is 'Sign Out'
+	//   fireEvent.click(LogoutButton);
+	//   expect(mockLogOut).toHaveBeenCalled(); // Check if the logout function is called
+	// });
 
-  test("link redirects to logout page", async () => {
-    render(<SettingsPage />);
-    const linkElement = screen.getAllByRole("link")[1];
-    expect(linkElement).toHaveAttribute("href", "/forgotpassword?loggedIn=true");
-  });
+	test("link redirects to logout page", async () => {
+		render(<SettingsPage />);
+		const linkElement = screen.getAllByRole("link")[1];
+		expect(linkElement).toHaveAttribute(
+			"href",
+			"/forgotpassword?loggedIn=true"
+		);
+	});
 });
