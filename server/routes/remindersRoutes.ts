20a1cec6eaf5eb959629522676101531cf7c2bf7
MrMelon1232 <julien.phan18@gmail.com>
2024-01-25 00:57:50 -0500

    removed reminders route file and dependencies of it in index file as its no longer used

--- server/routes/remindersRoutes.ts
+++
@@ -1,8 +0,0 @@
-// import { sendUserReminders } from "../tasks/reminderTask";
-// import enforceAuthorization from "../middlewares/enforceAuthorization";
-// import { Router } from "express";
-// const router = Router();
-
-// router.route("/").get(sendUserReminders);
-
-// export default router;
