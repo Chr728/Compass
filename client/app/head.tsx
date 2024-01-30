e9d7f1e94adc030438bdecf6caf9eb882ffa3609
Reuven1203 <reuven1203@gmail.com>
2024-01-30 18:09:12 -0500

    Disable user scalability

--- client/app/head.tsx
+++ client/app/head.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import {Metadata} from 'next';
 
 
 const Head = () => {
   return (
-    <>
-      <title>Compass</title>
-      <meta name="description" content="Compass" />
-      <meta name="author" content="Compass" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta charSet="UTF-8" />
-      <link rel="icon" href="" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="theme-color" color="black"></meta>
-    </>
+      <>
+          <title>Compass</title>
+          <meta name="description" content="Compass"/>
+          <meta name="author" content="Compass"/>
+          <meta charSet="UTF-8"/>
+          <link rel="icon" href=""/>
+          <link rel="manifest" href="/manifest.json"/>
+          <meta name="theme-color" color="red"></meta>
+      </>
   );
 };
 
