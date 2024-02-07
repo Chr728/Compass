83255cd063f77f497f8abaa893e7201a05477a57
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2024-02-06 20:00:47 -0500

    #392 Fixing conflicts

--- client/app/viewappointments/viewAppointmentsPage.tsx
+++ client/app/viewappointments/viewAppointmentsPage.tsx
@@ -179,132 +179,133 @@ export default function ViewAppointmentsPage() {
       };
 
   
-  return (
-    <div className="bg-eggshell min-h-screen flex flex-col w-full">
-          <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-2">
-            <button onClick={() => router.push('/health')} >
-                <Header headerText="Appointments"></Header>
-            </button>
-          </span>
-        <p 
-            className="text-grey font-sans text-[16px] mx-8">
-            Keep track of appointments you've set.
-        </p>
-        <div 
-            className="max-h-[500px] w-full rounded-3xl 
-            bg-white flex flex-col space-y-4 mt-8 
-            shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
-        >
-            <div className="flex space-x-2" style={{padding: '24px 16px 0 16px'}}>
-                <Button 
-                    type="button" 
-                    text="Add an item" 
-                    onClick={ () => router.push('/viewappointments/addappointment')} 
-                    style={{ 
-                        width: '100px', 
-                        height: '34px', 
-                        padding: '2px', 
-                        borderRadius: '3px', 
+      return (
+        <div className="bg-eggshell min-h-screen flex flex-col w-full">
+              <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-2">
+                <button onClick={() => router.push('/health')} >
+                    <Header headerText="Appointments"></Header>
+                </button>
+              </span>
+            <p 
+                className="text-grey font-sans text-[16px] mx-8">
+                Keep track of appointments you've set.
+            </p>
+            <div 
+                className="max-h-[500px] w-full rounded-3xl 
+                bg-white flex flex-col space-y-4 mt-8 
+                shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
+            >
+                <div className="flex space-x-2" style={{padding: '24px 16px 0 16px'}}>
+                    <Button 
+                        type="button" 
+                        text="Add an item" 
+                        onClick={ () => router.push('/viewappointments/addappointment')} 
+                        style={{ 
+                            width: '100px', 
+                            height: '34px', 
+                            padding: '2px', 
+                            borderRadius: '3px', 
+                            fontSize: '14px'
+                    }}/>
+                    <Button
+                        type="button"
+                        text="Daily"
+                        onClick={() => handleDailyClick()}
+                        style={{
+                        width: '100px',
+                        height: '34px',
+                        padding: '2px',
+                        borderRadius: '3px',
                         fontSize: '14px'
-                }}/>
-                <Button
-                    type="button"
-                    text="Daily"
-                    onClick={() => handleDailyClick()}
-                    style={{
-                    width: '100px',
-                    height: '34px',
-                    padding: '2px',
-                    borderRadius: '3px',
-                    fontSize: '14px'
-                }} />
-                <Button
-                    type="button"
-                    text="Monthly"
-                    onClick={() => handleCalendarClick()}
-                    style={{
-                    width: '100px',
-                    height: '34px',
-                    padding: '2px',
-                    borderRadius: '3px',
-                    fontSize: '14px'
-                }} />
-        </div>
-          
-        {!showCalendar &&
-            <div className='appointment h-[400px]'>
-                <TableContainer sx={{ maxHeight: 440,  }}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                        <TableRow>
-                            <TableCell>
-                                <div>
-                                    Date/Time
-                                    <button onClick={handleOrderDate} aria-label="orderDate">
-									{orderdate ? <MdKeyboardArrowUp className="inline-block text-lg text-darkgrey" /> : <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey"/>}
-								</button>
-                                </div>
-                            </TableCell>
-                            <TableCell>
-                                <div>
-                                Appointment
-                                <button onClick={handleOrderName} aria-label="orderName">
-									{ordername ? <MdKeyboardArrowUp className="inline-block text-lg text-darkgrey" /> : <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />  }
-                                </div>
-                            </TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        {data && Array.isArray(data) && data.map((row, index) => (
-                                    <TableRow 
-                                        onClick={() => handleClick(row.id)} 
-                                        style={rowStyles}
-                                    key={index}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {formatDate(row.date)},
-                                                <span className="font-bold"> {formatMilitaryTime(row.time)}</span>
-                                        </TableCell>
-                                        <TableCell >{row.appointmentWith}</TableCell>
-                                        <TableCell>
-                                            <Image 
-                                                src="/icons/trash.svg"
-                                                alt="Trash icon"
-                                                width={10}
-                                                height={10}
-                                                className="mr-4 md:hidden"
-                                                style={{ width: 'auto', height: 'auto' }}
-                                                onClick={(event) => {event.stopPropagation();handleDelete(row.id)}}
-                                            />                              
-                                        </TableCell>
-                                    </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                    }} />
+                    <Button
+                        type="button"
+                        text="Monthly"
+                        onClick={() => handleCalendarClick()}
+                        style={{
+                        width: '100px',
+                        height: '34px',
+                        padding: '2px',
+                        borderRadius: '3px',
+                        fontSize: '14px'
+                    }} />
             </div>
-        }
-        {
-          showCalendar &&
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateCalendar slotProps={{
-                        day: {
-                          highlightedDays,
-                        },
-                }}
-                slots={{
-                  day: ServerDay,
-                }}
-                style={{ background: 'white', color: 'black', marginLeft: '12px' }}
-              />
-            </LocalizationProvider>
-        }
-
-      </div>
-    </div>
-  )
-}
+            
+            {!showCalendar &&
+                <div className='appointment h-[400px]'>
+                    <TableContainer sx={{ maxHeight: 440,  }}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    <div>
+                                        Date/Time
+                                        <button onClick={handleOrderDate} aria-label="orderDate">
+									{orderdate ? <MdKeyboardArrowUp className="inline-block text-lg text-darkgrey" /> : <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />  }
+								</button>
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    <div>
+                                    Appointment
+                                    <button onClick={handleOrderName} aria-label="orderName">
+									{ordername ? <MdKeyboardArrowUp className="inline-block text-lg text-darkgrey" /> : <MdKeyboardArrowDown className="inline-block text-lg text-darkgrey" />  }
+							</button>
+                                    </div>
+                                </TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {data && Array.isArray(data) && data.map((row, index) => (
+                                        <TableRow 
+                                            onClick={() => handleClick(row.id)} 
+                                            style={rowStyles}
+                                        key={index}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {formatDate(row.date)},
+                                                    <span className="font-bold"> {formatMilitaryTime(row.time)}</span>
+                                            </TableCell>
+                                            <TableCell >{row.appointmentWith}</TableCell>
+                                            <TableCell>
+                                                <Image 
+                                                    src="/icons/trash.svg"
+                                                    alt="Trash icon"
+                                                    width={10}
+                                                    height={10}
+                                                    className="mr-4 md:hidden"
+                                                    style={{ width: 'auto', height: 'auto' }}
+                                                    onClick={(event) => {event.stopPropagation();handleDelete(row.id)}}
+                                                />                              
+                                            </TableCell>
+                                        </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            }
+            {
+              showCalendar &&
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateCalendar slotProps={{
+                            day: {
+                              highlightedDays,
+                            },
+                    }}
+                    slots={{
+                      day: ServerDay,
+                    }}
+                    style={{ background: 'white', color: 'black', marginLeft: '12px' }}
+                  />
+                </LocalizationProvider>
+            }
+    
+          </div>
+        </div>
+      )
+    }
 
 
