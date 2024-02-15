6919195a89e58972db4d733e2a243033bc48af0d
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2024-02-15 00:02:02 -0500

    Design initial UI for user with no data #412
    Created the initial user interface for users who have no existing data. This includes the option to import previously exported data or start fresh by adding a new folder. The design incorporates a button to add a folder.
    Note: This commit only includes the UI

---
+++ client/app/getMedVault/getMedVaultPage.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import { useState } from 'react';
+
+export default function GetMedVaultPage() {
+  const router = useRouter();
+  const [isExportDisabled, setIsExportDisabled] = useState(false);
+  return (
+    <div className="bg-eggshell h-screen flex flex-col">
+      <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-4">
+        <button onClick={() => router.push('/health')}>
+          <Header headerText="MedVault"></Header>
+        </button>
+      </span>
+      <div className="rounded-3xl flex flex-col mt-4 mb-44 w-full md:max-w-[800px] md:min-h-[550px] p-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <Button
+              type="button"
+              icon="/Upload.svg"
+              outlined={true}
+              text="Export Data"
+              style={{ width: '120px', fontSize: '14px' }}
+              disabled={isExportDisabled}
+            />
+          </div>
+          <div>
+            <Button
+              type="button"
+              icon="/Add.svg"
+              text="Add Folder"
+              style={{
+                width: '120px',
+                fontSize: '14px',
+                float: 'right',
+              }}
+            />
+          </div>
+        </div>
+        <img
+          src="/compass-removebg.png"
+          alt="Logo"
+          className="smallImage m-auto"
+          width={250}
+          height={250}
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        />
+        <div
+          className="bg-white rounded-lg p-4 shadow-2xl flex items-center justify-center cursor-pointer"
+          onClick={() => {
+            // Handle button click action here
+          }}
+        >
+          <div className="text-center flex flex-col items-center">
+            <img src="/Download.svg" alt="Download" className="w-12 h-12" />
+            <h3 className="font-semibold text-lg text-darkgrey">Import Data</h3>
+            <p className="text-sm text-darkgrey mt-4">
+              Import previously exported data or Add Folder
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/app/health/HealthPage.tsx
+++ client/app/health/HealthPage.tsx
@@ -6,82 +6,113 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import appointments from '../../public/appointments.svg';
 import medications from '../../public/medications.svg';
+import medVault from '../../public/documents.svg';
 import Header from '../components/Header';
 import Custom403 from '../pages/403';
-
 
 export default function Health() {
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
-    if (!user){
-      router.push("/login")
+    if (!user) {
+      router.push('/login');
     }
-}, [user]);
+  }, [user]);
 
-if (!user) {
-  return <div><Custom403/></div>
-}
-  
+  if (!user) {
+    return (
+      <div>
+        <Custom403 />
+      </div>
+    );
+  }
+
   return (
     <div className="bg-eggshell p-2 min-h-screen flex flex-col">
-        <div className="mb-10 flex flex-col w-full p-2">
-        <div style={{ marginTop: '-5%' }} >
-        <button className="mt-3 mb-4" onClick={() => router.push('/tpage')}>
-          <Header  headerText="Your Health"></Header>
-        </button>
-        
-      <p className="text-darkgrey mb-4">Let Compass remove the hassle of recalling when to take your medications and 
-          when you have important medical appointments.</p>   
-    
-      <Link href="/viewappointments">
-            <div className="rounded-3xl  relative flex flex-col  w-full md:max-w-[800px] md:h-[600px] p-5 " style={{backgroundColor: '#0880AE' }}>
-              <div> 
+      <div className="mb-10 flex flex-col w-full p-2">
+        <div style={{ marginTop: '-5%' }}>
+          <button className="mt-3 mb-4" onClick={() => router.push('/tpage')}>
+            <Header headerText="Your Health"></Header>
+          </button>
+
+          <p className="text-darkgrey mb-4">
+            Let Compass remove the hassle of recalling when to take your
+            medications and when you have important medical appointments.
+          </p>
+
+          <Link href="/viewappointments">
+            <div
+              className="rounded-3xl  relative flex flex-col  w-full md:max-w-[800px] md:h-[600px] p-5 "
+              style={{ backgroundColor: '#0880AE' }}
+            >
+              <div>
                 <p className="text-[18px] text-white font-IBM Plex Sans font-bold text-start">
                   Appointments
                 </p>
                 <p className="text-[14px] text-white pr-20 font-IBM Plex Sans  text-start">
-                Reminders for important events.
+                  Reminders for important events.
                 </p>
                 <NextImage
-        src = {appointments}
-        alt="appointments"
-        height={ 100 }
-        className="absolute inset-y-0 right-4 shrink-0"
-      />
-              
+                  src={appointments}
+                  alt="appointments"
+                  height={100}
+                  className="absolute inset-y-0 right-4 shrink-0"
+                />
               </div>
             </div>
-      </Link>
-      <br></br>
+          </Link>
+          <br></br>
 
-
-
-      <Link href="/getMedications">
-            <div className="rounded-3xl  relative flex flex-col  w-full md:max-w-[800px] md:h-[600px] p-5 " style={{backgroundColor: 'var(--Black, #2C2738)' }}>
-              
+          <Link href="/getMedications">
+            <div
+              className="rounded-3xl  relative flex flex-col  w-full md:max-w-[800px] md:h-[600px] p-5 "
+              style={{ backgroundColor: 'var(--Black, #2C2738)' }}
+            >
               <div>
                 <p className="text-[18px] text-white font-IBM Plex Sans font-bold text-start">
-                Medications
+                  Medications
                 </p>
                 <p className="text-[14px] text-white font-IBM Plex Sans  text-start">
-                Never miss a crucial dose.
+                  Never miss a crucial dose.
                 </p>
-                 <NextImage
-        src = {medications}
-        alt="medications"
-        width={88}
-        height={ 106 }
-        className="absolute inset-y-0 right-4 shrink-0"
-      />
+                <NextImage
+                  src={medications}
+                  alt="medications"
+                  width={88}
+                  height={106}
+                  className="absolute inset-y-0 right-4 shrink-0"
+                />
               </div>
             </div>
-      </Link>
-      <br></br>
+          </Link>
+          <br></br>
 
+          <Link href="/getMedVault">
+            <div
+              className="rounded-3xl  relative flex flex-col  w-full md:max-w-[800px] md:h-[600px] p-5 "
+              style={{ backgroundColor: 'var(--Black, #7F5FA5)' }}
+            >
+              <div>
+                <p className="text-[18px] text-white font-IBM Plex Sans font-bold text-start">
+                  MedVault
+                </p>
+                <p className="text-[14px] text-white font-IBM Plex Sans  text-start">
+                  Store your medical documents in one place.
+                </p>
+                <NextImage
+                  src={medVault}
+                  alt="documents"
+                  width={88}
+                  height={106}
+                  className="absolute inset-y-0 right-1 shrink-0"
+                />
+              </div>
+            </div>
+          </Link>
+          <br></br>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 }
