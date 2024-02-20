d5f977ffc493fd275e849da698c515c729a0117d
jonabitbol <tbnrsamurai@gmail.com>
2024-02-20 15:13:13 -0500

    #404 added location disabled info page

--- client/app/clinicLocator/clinicLocator.tsx
+++ client/app/clinicLocator/clinicLocator.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { useRouter } from 'next/navigation';
 import Button from "../components/Button"
-import fetchLocations from "@/app/http/fetchLocations";
+import Header from "../components/Header";
 
 type Location = {
     latitude: number;
@@ -13,6 +14,7 @@ export default function LocateClinic() {
         = useState<Location>(null);
     const [locationError, setLocationError]
         = useState("");
+    const router = useRouter();
 
     const onClick = () => {
 
@@ -34,20 +36,48 @@ export default function LocateClinic() {
             {location ? (
                 // Display Selection of Clinics
                 // If it keeps resetting before getting the chance to fetch the clinics, add local storage.
-                <p>Location is lat: {location.latitude}, lon: {location.longitude}</p>
+                <div>
+                    Location is lat: {location.latitude}, lon: {location.longitude}
+                </div>
             ): (
-                <Button
-                    type="button"
-                    text="Enable Location"
-                    onClick={onClick}
-                    style={{
-                        width: '150px',
-                        height: '50px',
-                        padding: '2px',
-                        borderRadius: '5px',
-                        fontSize: '14px'
-                    }}
-                />
+                <div className="bg-eggshell min-h-screen flex flex-col">
+                    <span className="flex items-baseline font-bold text-darkgrey text-[24px] mx-4 mt-4 mb-4">
+                        {/*TODO: make sure this router works*/}
+                        <button onClick={() => router.push('/health')}>
+                        <Header headerText="Locate Clinic"></Header>
+                        </button>
+                    </span>
+                    <div className="flex flex-col items-center">
+
+                        <div>
+                            <img
+                                src="/location-disabled.png"
+                                alt="Location Disabled"
+                                className="w-85 mt-5 drop-shadow-lg"
+                            />
+                        </div>
+
+                        <p className="font-sans text-darkgrey ml-5 text-[20px] font-bold mt-10 text-center px-5">
+                            Enable location services to use this feature
+                        </p>
+
+                        <Button
+                            type="button"
+                            text="Enable Location"
+                            onClick={onClick}
+                            style={{
+                                width: '150px',
+                                height: '50px',
+                                padding: '2px',
+                                borderRadius: '5px',
+                                fontSize: '14px',
+                                marginTop: '30px'
+                            }}
+                        />
+                    </div>
+                </div>
+
+
             )}
             {locationError && <p className="error">{locationError}</p>}
         </div>
