b2c92974c81c1406df4fa503995f9a45e2c6e0d7
Christina Darstbanian <minasderstep@gmail.com>
2024-02-22 02:41:57 -0500

    #206 putting the profile picture in different spot and redesigning the page

--- client/app/profile/profilePage.tsx
+++ client/app/profile/profilePage.tsx
@@ -23,88 +23,88 @@ export default function Profile() {
 	}, [userInfo, router]);
 
 	return (
-		<div className="bg-eggshell min-h-screen flex flex-col ">
-			<button onClick={() => router.push("/settings")}>
-				<div>
-					<Header headerText="View Profile"></Header>
+		<div>
+			<div
+				className="bg-eggshell min-h-screen flex flex-col "
+				style={{
+					minHeight: "600px",
+					height: "auto",
+					backgroundImage: "url('/profile.svg')",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+				}}>
+				<button onClick={() => router.push("/settings")}>
+					<div>
+						<Header headerText="View Profile"></Header>
+					</div>
+				</button>
+				{profile && (
+					<span className="rounded-2xl mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
+						<div className="mt-3 relative">
+							{profile.sex === "female" && (
+								<div className="flex justify-center">
+									<img
+										src="/female.jpg"
+										alt="Female"
+										className="rounded-full mt-1 "
+										height={100}
+										width={100}
+									/>
+								</div>
+							)}
+							{profile.sex === "male" && (
+								<div className="flex justify-center">
+									<img
+										src="/male.jpg"
+										alt="Male"
+										className="rounded-full mt-1"
+										height={100}
+										width={100}
+									/>
+								</div>
+							)}
+
+							<SingleEntry
+								label={"First Name :"}
+								value={profile.firstName}></SingleEntry>
+							<SingleEntry
+								label={"Last Name :"}
+								value={profile.lastName}></SingleEntry>
+							<SingleEntry
+								label={"Email :"}
+								value={profile.email}></SingleEntry>
+							<SingleEntry
+								label={"Phone number :"}
+								value={profile.phoneNumber}></SingleEntry>
+							<SingleEntry
+								label={"Birth Date :"}
+								value={
+									profile.birthDate
+										? new Date(profile.birthDate)
+												.toISOString()
+												.split("T")[0]
+										: ""
+								}></SingleEntry>
+							<SingleEntry
+								label={"Sex :"}
+								value={profile.sex}></SingleEntry>
+						</div>
+					</span>
+				)}
+				<div className="flex justify-center mt-2">
+					<Link href="/editprofile">
+						<Button
+							type="submit"
+							text="Edit Profile"
+							style={{
+								width: "180px",
+								alignContent: "center",
+							}}
+						/>
+					</Link>
 				</div>
-			</button>
-			{profile && (
-				// <span className="rounded-2xl mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
-				<span
-					className="rounded-2xl mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]"
-					style={{
-						minHeight: "500px",
-						height: "auto",
-						backgroundImage: "url('/profile.svg')",
-						backgroundSize: "cover",
-						backgroundPosition: "center",
-						backgroundRepeat: "no-repeat",
-					}}>
-					<div className="mt-3 relative">
-						{profile.sex === "female" && (
-							<div className="flex justify-center">
-								<img
-									src="/female.jpg"
-									alt="Female"
-									className="rounded-full mt-1 "
-									height={100}
-									width={100}
-								/>
-							</div>
-						)}
-						{profile.sex === "male" && (
-							<div className="flex justify-center">
-								<img
-									src="/male.jpg"
-									alt="Male"
-									className="rounded-full mt-1"
-									height={100}
-									width={100}
-								/>
-							</div>
-						)}
-
-						<SingleEntry
-							label={"First Name :"}
-							value={profile.firstName}></SingleEntry>
-						<SingleEntry
-							label={"Last Name :"}
-							value={profile.lastName}></SingleEntry>
-						<SingleEntry
-							label={"Email :"}
-							value={profile.email}></SingleEntry>
-						<SingleEntry
-							label={"Phone number :"}
-							value={profile.phoneNumber}></SingleEntry>
-						<SingleEntry
-							label={"Birth Date :"}
-							value={
-								profile.birthDate
-									? new Date(profile.birthDate)
-											.toISOString()
-											.split("T")[0]
-									: ""
-							}></SingleEntry>
-						<SingleEntry
-							label={"Sex :"}
-							value={profile.sex}></SingleEntry>
-					</div>
-
-					<div className="flex justify-center mt-6">
-						<Link href="/editprofile" className="mt-6">
-							<Button
-								type="submit"
-								text="Edit Profile"
-								style={{
-									width: "180px",
-									alignContent: "center",
-								}}
-							/>
-						</Link>
-					</div>
-				</span>
-			)}
+			</div>
 		</div>
 	);
 }
