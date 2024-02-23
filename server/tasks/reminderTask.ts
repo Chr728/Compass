0d12419ea6660f122b0d48932d8f0773cee4e614
MrMelon1232 <julien.phan18@gmail.com>
2024-02-23 18:48:29 -0500

    switched back expiration reminder time to 4am

--- server/tasks/reminderTask.ts
+++ server/tasks/reminderTask.ts
@@ -528,7 +528,7 @@ export const sendUserReminders = async () => {
           // Send notification to users if medication is about to expire
           if (moment(medication.expirationDate).isSame(currentDate, "day")) {
             // If the time is 4am, then send the medication expiration reminder
-            if (startTime.format("HH:mm:00") === "16:00:00") {
+            if (startTime.format("HH:mm:00") === "4:00:00") {
               const payload = JSON.stringify({
                 title: `Medication Expiration Reminder: ${medication.medicationName} for ${medication.dosage} ${medication.unit} at ${medication.time} is expiring today.`,
               });
