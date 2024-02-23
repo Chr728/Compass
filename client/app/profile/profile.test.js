115067a72d8d5ba509909f23c61405bd4799f0e7
Christina Darstbanian <minasderstep@gmail.com>
2024-02-23 01:50:50 -0500

    #206 redesigned profile page background image and added appropriate tests

--- client/app/profile/profile.test.js
+++ client/app/profile/profile.test.js
@@ -1,79 +1,117 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
-import ProfilePage from './profilePage';
-import { useRouter } from 'next/navigation';
-import {useUser} from '../contexts/UserContext';
-import {UserProvider} from '../contexts/UserContext';
+import ProfilePage from "./profilePage";
 
-
-const mockRouter= jest.fn();
+const mockRouter = jest.fn();
 
 jest.mock("next/navigation", () => ({
-    useRouter: () => {
-        return {
-            push: mockRouter
-        }
-    }
+	useRouter: () => {
+		return {
+			push: mockRouter,
+		};
+	},
 }));
 
-jest.mock('../contexts/UserContext', () => {
-    const originalModule = jest.requireActual('../contexts/UserContext');
+jest.mock("../contexts/UserContext", () => {
+	const originalModule = jest.requireActual("../contexts/UserContext");
 
-    return {
-        ...originalModule,
-        useUser: jest.fn(), // Mock the useUser hook
-        UserProvider:({ children }) => {
-            // Mock the UserProvider component
-            const userInfo = {
-                id: 1,
-                uid: 'mock-uid',
-                email: 'mock@example.com',
-                firstName: 'John',
-                lastName: 'Doe',
-                phoneNumber: '123-456-7890',
-                birthDate: new Date(),
-                sex: 'Male',
-            };
+	return {
+		...originalModule,
+		useUser: jest.fn(), // Mock the useUser hook
+		UserProvider: ({ children }) => {
+			// Mock the UserProvider component
+			const userInfo = {
+				id: 1,
+				uid: "mock-uid",
+				email: "mock@example.com",
+				firstName: "John",
+				lastName: "Doe",
+				phoneNumber: "123-456-7890",
+				birthDate: new Date(),
+				sex: "Male",
+			};
 
-            return <div>{children}</div>; // You can adjust this mock component as needed
-        },
-    };
+			return <div>{children}</div>; // You can adjust this mock component as needed
+		},
+	};
 });
 
+test("All fields are tested if visible to the user", () => {
+	require("../contexts/UserContext").useUser.mockReturnValue({
+		userInfo: {
+			id: 1,
+			uid: "mock-uid",
+			email: "mock@example.com",
+			firstName: "John",
+			lastName: "Doe",
+			phoneNumber: "123-456-7890",
+			birthDate: "1990-01-01",
+			sex: "male",
+		},
+	});
+	render(<ProfilePage />);
+	expect(screen.getByText("First Name :")).toBeInTheDocument();
+	expect(screen.getByText("John")).toBeInTheDocument();
 
+	expect(screen.getByText("Last Name :")).toBeInTheDocument();
+	expect(screen.getByText("Doe")).toBeInTheDocument();
 
+	expect(screen.getByText("Email :")).toBeInTheDocument();
+	expect(screen.getByText("mock@example.com")).toBeInTheDocument();
 
-    test("All fields are tested if visible to the user", () => {
-        require('../contexts/UserContext').useUser.mockReturnValue({
-            userInfo: {
-                id: 1,
-                uid: 'mock-uid',
-                email: 'mock@example.com',
-                firstName: 'John',
-                lastName: 'Doe',
-                phoneNumber: '123-456-7890',
-                birthDate: "1990-01-01",
-                sex: 'Male',
-            },
-        });
-        render(<ProfilePage/>);
-        expect(screen.getByText("First Name :")).toBeInTheDocument();
-        expect(screen.getByText("John")).toBeInTheDocument();
+	expect(screen.getByText("Phone number :")).toBeInTheDocument();
+	expect(screen.getByText("123-456-7890")).toBeInTheDocument();
 
-        expect(screen.getByText("Last Name :")).toBeInTheDocument();
-        expect(screen.getByText("Doe")).toBeInTheDocument();
+	expect(screen.getByText("Birth Date :")).toBeInTheDocument();
+	expect(screen.getByText("1990-01-01")).toBeInTheDocument();
 
-        expect(screen.getByText("Email :")).toBeInTheDocument();
-        expect(screen.getByText("mock@example.com")).toBeInTheDocument();
+	expect(screen.getByText("Sex :")).toBeInTheDocument();
+	expect(screen.getByText("male")).toBeInTheDocument();
+});
 
-        expect(screen.getByText("Phone number :")).toBeInTheDocument();
-        expect(screen.getByText("123-456-7890")).toBeInTheDocument();
+test("profile page background image is displayed", () => {
+	render(<ProfilePage />);
+	const backgroundImage = screen.getByTestId("profile-background-image");
+	expect(backgroundImage).toBeInTheDocument();
+});
 
-        expect(screen.getByText("Birth Date :")).toBeInTheDocument();
-        expect(screen.getByText("1990-01-01")).toBeInTheDocument();
+// test("Logo displayed correctly", async () => {
+// 	require("../contexts/UserContext").useUser.mockReturnValue({
+// 		userInfo: {
+// 			id: 1,
+// 			uid: "mock-uid",
+// 			email: "mock@example.com",
+// 			firstName: "John",
+// 			lastName: "Doe",
+// 			phoneNumber: "123-456-7890",
+// 			birthDate: "1990-01-01",
+// 			sex: "Male",
+// 		},
+// 	});
+// 	render(<ProfilePage />);
+// 	const maleImage = screen.getByAltText("Male");
+// 	expect(maleImage).toBeVisible();
+// });
+// test("Logo displayed correctly", async () => {
+// 	render(<ProfilePage />); // Render the ProfilePage component with a male profile
 
-        expect(screen.getByText("Sex :")).toBeInTheDocument();
-        expect(screen.getByText("Male")).toBeInTheDocument();
+// 	// Wait for the image to be present in the DOM
+// 	const maleImage = await screen.findByTestId("male-image");
 
-    })
+// 	// Assert that the male image is visible
+// 	expect(maleImage).toBeVisible();
+// });
 
+test("Male image displayed correctly", async () => {
+	// Mock profile data with sex as "male"
+	const profile = { sex: "male" };
+
+	// Render the ProfilePage component with the mock profile data
+	render(<ProfilePage profile={profile} />);
+
+	// Find the male image by its alt text
+	const maleImage = await screen.findByAltText("Male");
+
+	// Assert that the male image is visible
+	expect(maleImage).toBeVisible();
+});
--- client/app/profile/profilePage.tsx
+++ client/app/profile/profilePage.tsx
@@ -27,20 +27,21 @@ export default function Profile() {
 			<div
 				className="bg-eggshell min-h-screen flex flex-col "
 				style={{
-					minHeight: "600px",
-					height: "auto",
+					minHeight: "800px",
+					maxHeight: "auto",
 					backgroundImage: "url('/profile.svg')",
 					backgroundSize: "cover",
 					backgroundPosition: "center",
 					backgroundRepeat: "no-repeat",
-				}}>
+				}}
+				data-testid="profile-background-image">
 				<button onClick={() => router.push("/settings")}>
 					<div>
 						<Header headerText="View Profile"></Header>
 					</div>
 				</button>
 				{profile && (
-					<span className="rounded-2xl mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
+					<span className="rounded-3xl mt-6 mb-10 mr-28 bg-white flex flex-col m-auto w-full md:max-w-[800px] md:min-h-[600px] p-8 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
 						<div className="mt-3 relative">
 							{profile.sex === "female" && (
 								<div className="flex justify-center">
@@ -61,6 +62,7 @@ export default function Profile() {
 										className="rounded-full mt-1"
 										height={100}
 										width={100}
+										data-testid="male-image"
 									/>
 								</div>
 							)}
