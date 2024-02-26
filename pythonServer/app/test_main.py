1093867dce98ec60cbc6062bbd17aefbbecdc13c
Jiayi Chen <cjy0427615@gmail.com>
2024-02-25 20:11:24 -0500

    added test for mp3 format file #431
    the test file is test_audio_long.mp3

--- pythonServer/app/test_main.py
+++ pythonServer/app/test_main.py
@@ -2,7 +2,6 @@ from fastapi.testclient import TestClient
 from fastapi import HTTPException
 import pytest
 from unittest.mock import MagicMock
-
 from .main import app
 from . import main
 
@@ -12,6 +11,11 @@ def fake_predict(filename):
     raise Exception("new Exception")
 
 original_predict= main.predict
+
+def fake_load_audio_to_tensor(file, type):
+    raise Exception("new Exception")
+
+original_load_audio_to_tensor= main.load_audio_to_tensor
 
 def test_read_main():
     response = client.get("/")
@@ -43,5 +47,11 @@ def test_test():
     assert response.json() == {'detail': 'Error: HTTPException'}
     main.predict=original_predict
 
+
+def test_snoring_AI_mp3():
+    test_file = './test_files/test_audio_long.mp3'
+    files = {'file': ('test_audio_long.mp3', open(test_file, 'rb'))}
+    response = client.post('/SnoringAI', files=files)
+    assert response.status_code == 200
 
 
