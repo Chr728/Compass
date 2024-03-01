345a0ab6efe69a9c5aadd80af3fcae41ee5ed194
Christina Darstbanian <minasderstep@gmail.com>
2024-02-29 21:12:55 -0500

    #415 renaming the folder sturcture in get oxygen journals to make it suitable for routing among pages

--- client/app/getOxygenJournals/page.tsx
+++ client/app/getOxygenJournals/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
 import { useAuth } from "../contexts/AuthContext";
-import GetWeightJournalsPage from "./getWeightJournalsPage";
+import GetOxygenJournalsPage from "./getOxygenJournalsPage";
 
-export default function GetWeightJournals() {
+export default function GetOxygenJournals() {
 	const router = useRouter();
 	const { user } = useAuth();
 
-	// React.useEffect(() => {
-	// 	if (!user) router.push("/login");
-	// }, [user]);
-
-	// if (!user) {
-	//   return <div><Custom403/></div>
-	// }
-
-	return <GetWeightJournalsPage />;
+	return <GetOxygenJournalsPage />;
 }
