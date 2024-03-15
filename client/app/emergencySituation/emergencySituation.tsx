16f0957f07f18e1a25b8e32d711195ff6fa9a5a8
Christina Darstbanian <minasderstep@gmail.com>
2024-03-15 11:55:55 -0400

    #459 verifying the fetching of API call

--- client/app/emergencySituation/emergencySituation.tsx
+++ client/app/emergencySituation/emergencySituation.tsx
@@ -1,20 +1,41 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { useAuth } from "../contexts/AuthContext";
+import { useProp } from "../contexts/PropContext";
 import { useUser } from "../contexts/UserContext";
-
+import { getEmergencySituation } from "../http/emergencySituationAPI";
 export default function AboutUsPage() {
 	const logger = require("../../logger");
 	const router = useRouter();
-	const { userInfo } = useUser();
+	const { user } = useAuth();
 
+	const { userInfo } = useUser();
+	const [emergency, setemergency] = useState<any>(null);
+	const { handlePopUp } = useProp();
 	useEffect(() => {
 		if (!userInfo) {
 			logger.warn("User not found.");
 			alert("User not found.");
 		}
 	}, [userInfo, router]);
+
+	useEffect(() => {
+		async function fetchEmergencyInfo() {
+			try {
+				const userId = user?.uid || "";
+				const result = await getEmergencySituation();
+				logger.info("All info retrieved:", result);
+				setemergency(result.data);
+			} catch (error) {
+				handlePopUp("error", "Error retrieving info:");
+			}
+		}
+		setTimeout(() => {
+			fetchEmergencyInfo();
+		}, 500);
+	}, [user]);
 
 	return (
 		<div className="bg-eggshell min-h-screen flex flex-col">
@@ -23,40 +44,27 @@ export default function AboutUsPage() {
 					<Header headerText="Emergency Rooms"></Header>
 				</button>
 			</span>
-
-			<div className="rounded-3xl bg-white flex flex-col mt-4 mb-44 w-full md:max-w-[800px] md:min-h-[550px] p-4 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
-				<div className="font-bold text-start text-darkgrey p-3 text-[20px]">
-					Last Updated:
-				</div>
-				<div className="flex justify-between items-center">
-					<br></br> <br></br>
-					<div className="font-sans text-start text-darkgrey p-4 text-[14px]">
-						Compass is a medical wellness app that targets
-						specifically people above 40 years old who might be
-						interested in having some type of assistance to keep
-						their healthy habits and lifestyles. Compass offers
-						features of managing medical reminders, booking
-						appointments,tracking user’s medications and treatments
-						all in one consolidated application. In addition, having
-						features of medical journals such as diabetic journals
-						allows some patients to easily note their daily doses
-						and treatment details making them able to follow the
-						history of their treatments and use it as a reference
-						for themselves or to show to their medical professional.
-						Additionally,with the speed dial fast option to contact
-						relatives during some emergency situations patients
-						would be able to contact their relatives in a faster and
-						easier way. With many features compass aims for users to
-						be healthier and function hassle free.
-						<p
-							className={
-								"w-full h-full text-grey text-center mt-32"
-							}>
-							&copy; Compass 2024
-						</p>
-					</div>
-				</div>
+			<div className="font-bold text-start text-darkgrey p-3 text-[20px]">
+				Last Updated
 			</div>
+			{emergency &&
+				emergency.map((item: any, index: number) => (
+					<div className="rounded-3xl bg-white flex flex-col mt-4 mb-44 w-full md:max-w-[800px] md:min-h-[550px] p-4 shadow-[0_32px_64px_0_rgba(44,39,56,0.08),0_16px_32px_0_rgba(44,39,56,0.04)]">
+						<div
+							key={item.hospital_name}
+							className={`flex justify-between items-center mt-3`}
+							style={{
+								backgroundColor:
+									index % 2 === 0 ? "white" : "#DBE2EA",
+							}}>
+							<div className="flex-2">
+								<p className="font-sans font-medium text-darkgrey text-[14px] text-center">
+									{item.hospital_name}
+								</p>
+							</div>
+						</div>
+					</div>
+				))}
 		</div>
 	);
 }
---
+++ client/app/http/emergencySituationAPI.ts
@@ -0,0 +1,41 @@
+import { auth } from "../config/firebase";
+const logger = require("../../logger");
+
+// Function to make a GET request to retrieve all emergency rooms
+export async function getEmergencySituation(): Promise<any> {
+	try {
+		const currentUser = auth.currentUser;
+		if (!currentUser) {
+			logger.error("No user is currently signed in.");
+			throw new Error("No user is currently signed in.");
+		}
+		const id = currentUser.uid;
+		const token = await currentUser.getIdToken();
+
+		const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_URL}/api/emergencyRoomData/${id}`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		if (response.ok) {
+			logger.info(`info fetched successfully for user ${id}`);
+			const data = await response.json();
+			return data;
+		} else {
+			logger.error(
+				`Failed to retrieve emergency situation HTTP Status: ${response.status}`
+			);
+			throw new Error(
+				`Failed to retrieve  emergency situation  HTTP Status: ${response.status}`
+			);
+		}
+	} catch (error) {
+		logger.error("Error fetching  emergency situation :", error);
+		throw error;
+	}
+}
