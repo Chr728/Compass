891158ce58d1c89aa0e311a510a7c4f2def9e5d1
Christina Darstbanian <minasderstep@gmail.com>
2024-04-04 17:24:45 -0400

    #465 added labels to weight journal graphs

--- client/app/getWeightJournals/getWeightJournalsPage.tsx
+++ client/app/getWeightJournals/getWeightJournalsPage.tsx
@@ -94,6 +94,16 @@ export default function GetWeightJournalsPage() {
 								scales: {
 									y: {
 										beginAtZero: true,
+										title: {
+											display: true,
+											text: "Weight",
+										},
+									},
+									x: {
+										title: {
+											display: true,
+											text: "Row Index",
+										},
 									},
 								},
 							},
@@ -310,7 +320,7 @@ export default function GetWeightJournalsPage() {
 							</div>
 						</div>
 
-						<div className="flex-2" >
+						<div className="flex-2">
 							<div className="font-sans  text-darkgrey font-bold text-[18px] text-center">
 								BMI
 								<button
@@ -338,9 +348,7 @@ export default function GetWeightJournalsPage() {
 								</button>
 							</div>
 						</div>
-						<div
-							className="flex-2 mt-2" >
-							
+						<div className="flex-2 mt-2">
 							<input
 								type="checkbox"
 								checked={selectAll}
